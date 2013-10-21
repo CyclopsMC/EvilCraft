3e4c9cee9dd48a8a1a7c38311896d97cef0ecfb4
Ruben Taelman <rubensworks@gmail.com>
2013-10-21 20:59:23 +0200

    Added a HotbarIterator.

--- common/evilcraft/events/BloodObtainLivingDeathEventHook.java
+++ common/evilcraft/events/BloodObtainLivingDeathEventHook.java
@@ -2,7 +2,6 @@ package evilcraft.events;
 
 import java.util.Random;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
@@ -11,7 +10,7 @@ import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.ItemFluidContainer;
-import evilcraft.EvilCraft;
+import evilcraft.api.HotbarIterator;
 import evilcraft.items.BloodExtractor;
 import evilcraft.liquids.Blood;
 
@@ -23,19 +22,14 @@ public class BloodObtainLivingDeathEventHook {
         if(e != null && e instanceof EntityPlayerMP) {
             EntityPlayerMP player = (EntityPlayerMP) e;
             
-            ItemStack[] hotbar = player.inventory.mainInventory;
             int toFill = 100 + (new Random()).nextInt(900);
-            int i = 0;
-            while(i < hotbar.length && toFill > 0) {
-                ItemStack itemStack = hotbar[i];
+            HotbarIterator it = new HotbarIterator(player);
+            while(it.hasNext() && toFill > 0) {
+                ItemStack itemStack = it.next();
                 if(itemStack != null && itemStack.getItem() == BloodExtractor.getInstance()) {
-                    EvilCraft.log("Found BE!");
-                    //itemStack.getItem().setDamage(itemStack, 3);
                     ItemFluidContainer container = (ItemFluidContainer) itemStack.getItem();
                     toFill -= container.fill(itemStack, new FluidStack(Blood.getInstance(), toFill), true);
-                    EvilCraft.log("Content: "+container.getFluid(itemStack).amount);
                 }
-                i++;
             }
         }
         
