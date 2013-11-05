125b4dfe78c54ee79c794681815e7cad3e35cc36
Ruben Taelman <rubensworks@gmail.com>
2013-11-05 22:14:27 +0100

    Added recipe for the WeatherContainer.

--- common/evilcraft/Recipes.java
+++ common/evilcraft/Recipes.java
@@ -1,9 +1,12 @@
 package evilcraft;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 import evilcraft.blocks.DarkBlock;
+import evilcraft.items.ContainedFlux;
 import evilcraft.items.DarkGem;
+import evilcraft.items.WeatherContainer;
 
 /**
  * Holder class of all the recipes.
@@ -20,6 +23,15 @@ public class Recipes {
         // 1 DarkBlock -> 9 DarkGems
         GameRegistry.addShapelessRecipe(new ItemStack(DarkGem.getInstance(), 9),
                 new ItemStack(DarkBlock.getInstance())
+        );
+        // Weather Container
+        GameRegistry.addRecipe(new ItemStack(WeatherContainer.getInstance()),
+                " G ",
+                " P ",
+                " S ",
+                'G', new ItemStack(ContainedFlux.getInstance()),
+                'P', new ItemStack(Item.glassBottle),
+                'S', new ItemStack(Item.sugar)
         );
     }
 }
