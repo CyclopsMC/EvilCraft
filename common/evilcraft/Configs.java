e1b89c6f5962afbfce077e4c06e3639bd59dde9c
Ruben Taelman <rubensworks@gmail.com>
2013-12-29 20:12:29 +0100

    Moved configs to a better place to keep the main class file clean.

---
+++ common/evilcraft/Configs.java
@@ -0,0 +1,101 @@
+package evilcraft;
+
+import java.util.Set;
+
+import evilcraft.api.config.ConfigHandler;
+import evilcraft.api.config.ExtendedConfig;
+import evilcraft.blocks.BloodInfuserConfig;
+import evilcraft.blocks.BloodStainedBlockConfig;
+import evilcraft.blocks.BloodyCobblestoneConfig;
+import evilcraft.blocks.DarkBlockConfig;
+import evilcraft.blocks.DarkOreConfig;
+import evilcraft.blocks.EvilBlockConfig;
+import evilcraft.blocks.ExcrementPileConfig;
+import evilcraft.blocks.LargeDoorConfig;
+import evilcraft.blocks.LightningBombConfig;
+import evilcraft.blocks.LiquidBlockBloodConfig;
+import evilcraft.blocks.NetherfishSpawnConfig;
+import evilcraft.enchantment.EnchantmentBreakingConfig;
+import evilcraft.enchantment.EnchantmentLifeStealingConfig;
+import evilcraft.enchantment.EnchantmentUnusingConfig;
+import evilcraft.entities.item.EntityLightningGrenadeConfig;
+import evilcraft.entities.monster.NetherfishConfig;
+import evilcraft.entities.monster.WerewolfConfig;
+import evilcraft.fluids.BloodConfig;
+import evilcraft.items.BloodExtractorConfig;
+import evilcraft.items.BloodPearlOfTeleportationConfig;
+import evilcraft.items.BroomConfig;
+import evilcraft.items.BucketBloodConfig;
+import evilcraft.items.ContainedFluxConfig;
+import evilcraft.items.DarkGemConfig;
+import evilcraft.items.DarkStickConfig;
+import evilcraft.items.LargeDoorItemConfig;
+import evilcraft.items.LightningGrenadeConfig;
+import evilcraft.items.WeatherContainerConfig;
+import evilcraft.items.WerewolfBoneConfig;
+import evilcraft.items.WerewolfFleshConfig;
+
+public class Configs {
+    
+    private static Configs _instance;
+    
+    private Set<ExtendedConfig> configs = ConfigHandler.getInstance(); // Order is necessary for some registrations
+    
+    public static Configs getInstance() {
+        if(_instance == null)
+            _instance = new Configs();
+        return _instance;
+    }
+    
+    private Configs() {
+        
+    }
+    
+    public void registerConfigs() {
+        // General
+        configs.add(new GeneralConfig());
+        
+        // Fluids
+        configs.add(new BloodConfig());
+        
+        // Blocks
+        configs.add(new EvilBlockConfig());
+        configs.add(new LiquidBlockBloodConfig());
+        configs.add(new DarkOreConfig());
+        configs.add(new DarkBlockConfig());
+        configs.add(new BloodStainedBlockConfig());
+        configs.add(new LightningBombConfig());
+        configs.add(new LargeDoorConfig());
+        configs.add(new ContainedFluxConfig());
+        configs.add(new BloodInfuserConfig());
+        configs.add(new BloodyCobblestoneConfig());
+        configs.add(new NetherfishSpawnConfig());
+        configs.add(new ExcrementPileConfig());
+        
+        // Items
+        configs.add(new WerewolfBoneConfig());
+        configs.add(new WerewolfFleshConfig());
+        configs.add(new LightningGrenadeConfig());
+        configs.add(new BucketBloodConfig());
+        configs.add(new BloodExtractorConfig());
+        configs.add(new DarkGemConfig());
+        configs.add(new DarkStickConfig());
+        configs.add(new LargeDoorItemConfig());
+        configs.add(new WeatherContainerConfig());        
+        configs.add(new BloodPearlOfTeleportationConfig());
+        configs.add(new BroomConfig());
+        
+        // Entities
+        //Item
+        configs.add(new EntityLightningGrenadeConfig());
+        // Monster
+        configs.add(new WerewolfConfig()); // http://www.minecraftwiki.net/wiki/Resource_pack#pack.mcmeta
+        configs.add(new NetherfishConfig());
+        
+        // Enchantments
+        configs.add(new EnchantmentUnusingConfig());
+        configs.add(new EnchantmentBreakingConfig());
+        configs.add(new EnchantmentLifeStealingConfig());
+    }
+    
+}
--- common/evilcraft/EvilCraft.java
+++ common/evilcraft/EvilCraft.java
@@ -1,7 +1,6 @@
 package evilcraft;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 
 import net.minecraft.client.renderer.entity.Render;
@@ -17,41 +16,10 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import evilcraft.api.BucketHandler;
 import evilcraft.api.LoggerHelper;
 import evilcraft.api.config.ConfigHandler;
-import evilcraft.api.config.ExtendedConfig;
-import evilcraft.blocks.BloodInfuserConfig;
-import evilcraft.blocks.BloodStainedBlockConfig;
-import evilcraft.blocks.BloodyCobblestoneConfig;
-import evilcraft.blocks.DarkBlockConfig;
-import evilcraft.blocks.DarkOreConfig;
-import evilcraft.blocks.EvilBlockConfig;
-import evilcraft.blocks.ExcrementPileConfig;
-import evilcraft.blocks.LargeDoorConfig;
-import evilcraft.blocks.LightningBombConfig;
-import evilcraft.blocks.LiquidBlockBloodConfig;
-import evilcraft.blocks.NetherfishSpawnConfig;
-import evilcraft.enchantment.EnchantmentBreakingConfig;
-import evilcraft.enchantment.EnchantmentLifeStealingConfig;
-import evilcraft.enchantment.EnchantmentUnusingConfig;
-import evilcraft.entities.item.EntityLightningGrenadeConfig;
-import evilcraft.entities.monster.NetherfishConfig;
-import evilcraft.entities.monster.WerewolfConfig;
 import evilcraft.events.LivingAttackEventHook;
 import evilcraft.events.LivingDeathEventHook;
 import evilcraft.events.PlayerInteractEventHook;
 import evilcraft.events.TextureStitchEventHook;
-import evilcraft.fluids.BloodConfig;
-import evilcraft.items.BloodExtractorConfig;
-import evilcraft.items.BloodPearlOfTeleportationConfig;
-import evilcraft.items.BroomConfig;
-import evilcraft.items.BucketBloodConfig;
-import evilcraft.items.ContainedFluxConfig;
-import evilcraft.items.DarkGemConfig;
-import evilcraft.items.DarkStickConfig;
-import evilcraft.items.LargeDoorItemConfig;
-import evilcraft.items.LightningGrenadeConfig;
-import evilcraft.items.WeatherContainerConfig;
-import evilcraft.items.WerewolfBoneConfig;
-import evilcraft.items.WerewolfFleshConfig;
 import evilcraft.proxies.CommonProxy;
 import evilcraft.worldgen.EvilWorldGenerator;
 
@@ -64,53 +32,6 @@ public class EvilCraft {
     
     public static EvilCraft _instance;
     
-    private static Set<ExtendedConfig> configs = ConfigHandler.getInstance(); // Order is necessary for some registrations
-    static {
-        // General
-        configs.add(new GeneralConfig());
-        
-        // Fluids
-        configs.add(new BloodConfig());
-        
-        // Blocks
-        configs.add(new EvilBlockConfig());
-        configs.add(new LiquidBlockBloodConfig());
-        configs.add(new DarkOreConfig());
-        configs.add(new DarkBlockConfig());
-        configs.add(new BloodStainedBlockConfig());
-        configs.add(new LightningBombConfig());
-        configs.add(new LargeDoorConfig());
-        configs.add(new ContainedFluxConfig());
-        configs.add(new BloodInfuserConfig());
-        configs.add(new BloodyCobblestoneConfig());
-        configs.add(new NetherfishSpawnConfig());
-        configs.add(new ExcrementPileConfig());
-        
-        // Items
-        configs.add(new WerewolfBoneConfig());
-        configs.add(new WerewolfFleshConfig());
-        configs.add(new LightningGrenadeConfig());
-        configs.add(new BucketBloodConfig());
-        configs.add(new BloodExtractorConfig());
-        configs.add(new DarkGemConfig());
-        configs.add(new DarkStickConfig());
-        configs.add(new LargeDoorItemConfig());
-        configs.add(new WeatherContainerConfig());        
-        configs.add(new BloodPearlOfTeleportationConfig());
-        configs.add(new BroomConfig());
-        
-        // Entities
-        //Item
-        configs.add(new EntityLightningGrenadeConfig());
-        // Monster
-        configs.add(new WerewolfConfig()); // http://www.minecraftwiki.net/wiki/Resource_pack#pack.mcmeta
-        configs.add(new NetherfishConfig());
-        
-        // Enchantments
-        configs.add(new EnchantmentUnusingConfig());
-        configs.add(new EnchantmentBreakingConfig());
-        configs.add(new EnchantmentLifeStealingConfig());
-    }
     public static Map<Class<? extends Entity>, Render> renderers = new HashMap<Class<? extends Entity>, Render>();
     
     @EventHandler
@@ -121,7 +42,8 @@ public class EvilCraft {
         // Save this instance, so we can use it later
         this._instance = this;
         
-        // Run the ConfigHandler to make/read the config and fill in the game registry
+        // Register configs and run the ConfigHandler to make/read the config and fill in the game registry
+        Configs.getInstance().registerConfigs();
         ConfigHandler.getInstance().handle(event);
     }
     
