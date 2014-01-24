a32a478e279782ce8bd766dc2d0b17791a3a5228
Ruben Taelman <rubensworks@gmail.com>
2014-01-24 21:46:21 +0100

    The BloodChest will now add random bad enchants to the repairing items with a small chance, disableable in config.

--- common/evilcraft/blocks/BloodChestConfig.java
+++ common/evilcraft/blocks/BloodChestConfig.java
@@ -3,12 +3,17 @@ package evilcraft.blocks;
 import net.minecraft.item.ItemBlock;
 import evilcraft.Reference;
 import evilcraft.api.config.BlockConfig;
+import evilcraft.api.config.ElementTypeCategory;
+import evilcraft.api.config.configurable.ConfigurableProperty;
 import evilcraft.api.item.ItemBlockNBT;
 import evilcraft.entities.tileentities.TileBloodChest;
 import evilcraft.proxies.ClientProxy;
 import evilcraft.render.RenderBloodChest;
 
 public class BloodChestConfig extends BlockConfig {
+    
+    @ConfigurableProperty(category = ElementTypeCategory.GENERAL, comment = "If the Blood Chest should add random bad enchants with a small chance to repairing items.", isCommandable = true)
+    public static boolean addRandomBadEnchants = true;
     
     public static BloodChestConfig _instance;
 
--- common/evilcraft/entities/tileentities/tickaction/bloodchest/RepairItemTickAction.java
+++ common/evilcraft/entities/tileentities/tickaction/bloodchest/RepairItemTickAction.java
@@ -1,13 +1,24 @@
 package evilcraft.entities.tileentities.tickaction.bloodchest;
 
+import java.util.LinkedList;
+
 import net.minecraft.item.ItemStack;
+import evilcraft.api.config.configurable.ConfigurableEnchantment;
 import evilcraft.api.entities.tileentitites.tickaction.ITickAction;
+import evilcraft.blocks.BloodChestConfig;
+import evilcraft.enchantment.EnchantmentBreaking;
 import evilcraft.entities.tileentities.TileBloodChest;
 
 public class RepairItemTickAction implements ITickAction<TileBloodChest> {
     
     protected final static int MB_PER_DAMAGE = 10;
     protected final static int TICKS_PER_DAMAGE = 10;
+    
+    private static final int CHANCE_RANDOM_ENCHANT = 10000;
+    private static final LinkedList<ConfigurableEnchantment> BAD_ENCHANTS = new LinkedList<ConfigurableEnchantment>();
+    static {
+        BAD_ENCHANTS.add(EnchantmentBreaking.getInstance());
+    }
     
     @Override
     public boolean canTick(TileBloodChest tile, ItemStack itemStack, int slot, int tick) {
@@ -16,8 +27,21 @@ public class RepairItemTickAction implements ITickAction<TileBloodChest> {
     
     private void repair(TileBloodChest tile, ItemStack itemStack) {
         tile.getTank().drain(MB_PER_DAMAGE, true);
+        
+        // Repair the item
         int newDamage = itemStack.getItemDamage() - 1;
         itemStack.setItemDamage(newDamage);
+        
+        // Add bad enchant with a certain chance
+        if(BloodChestConfig.addRandomBadEnchants
+                && tile.getWorldObj().rand.nextInt(CHANCE_RANDOM_ENCHANT) == 0) {
+            ConfigurableEnchantment enchantment = BAD_ENCHANTS.get(tile.getWorldObj().rand.nextInt(BAD_ENCHANTS.size()));
+            itemStack.addEnchantment(
+                    enchantment,
+                    enchantment.getMinLevel() + tile.getWorldObj().rand.nextInt(
+                            enchantment.getMaxLevel() - enchantment.getMinLevel())
+                    );
+        }
     }
 
     @Override
