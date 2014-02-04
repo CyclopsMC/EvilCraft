7324d255704eea32b2e69f7d5b5ccb24d91f0d71
immortaleeb <immortaleeb@gmail.com>
2014-02-04 21:06:47 +0100

    Fixed Nullpointer when clicking on blood stained block

--- common/evilcraft/blocks/BloodStainedBlock.java
+++ common/evilcraft/blocks/BloodStainedBlock.java
@@ -92,6 +92,7 @@ public class BloodStainedBlock extends ConfigurableBlockWithInnerBlocks {
         }
     }
     
+    @SideOnly(Side.CLIENT)
     @Override
     public void onBlockClicked(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer)
     {
