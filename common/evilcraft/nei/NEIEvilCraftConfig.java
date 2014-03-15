75714d694313da4a9a8f2b640bef7696755b0b13
Ruben Taelman <rubensworks@gmail.com>
2014-03-15 16:16:08 +0100

    Added Blood Infuser NEI support.

---
+++ common/evilcraft/nei/NEIEvilCraftConfig.java
@@ -0,0 +1,30 @@
+package evilcraft.nei;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import evilcraft.Reference;
+
+/**
+ * Config for the NEI integration of this mod.
+ * @author rubensworks
+ *
+ */
+public class NEIEvilCraftConfig implements IConfigureNEI {
+
+    @Override
+    public String getName() {
+        return Reference.MOD_NAME;
+    }
+
+    @Override
+    public String getVersion() {
+        return Reference.MOD_VERSION;
+    }
+
+    @Override
+    public void loadConfig() {
+        API.registerRecipeHandler(new NEIBloodInfuserManager());
+        API.registerUsageHandler(new NEIBloodInfuserManager());
+    }
+
+}
