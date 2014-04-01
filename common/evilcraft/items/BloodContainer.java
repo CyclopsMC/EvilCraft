336eaee667f5d99bb59e7922863fe0b8bd103d80
Ruben Taelman <rubensworks@gmail.com>
2014-04-01 19:54:49 +0200

    Moved the logic for activation of items to ItemHelpers.

--- common/evilcraft/items/BloodContainer.java
+++ common/evilcraft/items/BloodContainer.java
@@ -7,7 +7,6 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
@@ -16,6 +15,7 @@ import net.minecraftforge.fluids.IFluidContainerItem;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import evilcraft.api.IInformationProvider;
+import evilcraft.api.ItemHelpers;
 import evilcraft.api.config.ExtendedConfig;
 import evilcraft.api.config.ItemConfig;
 import evilcraft.api.config.configurable.ConfigurableDamageIndicatedItemFluidContainer;
@@ -94,7 +94,7 @@ public class BloodContainer extends ConfigurableDamageIndicatedItemFluidContaine
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
         if(player.isSneaking()) {
             if(!world.isRemote)
-                toggleContainerActivation(itemStack);
+                ItemHelpers.toggleActivation(itemStack);
             return itemStack;
         }
         return super.onItemRightClick(itemStack, world, player);
@@ -102,7 +102,7 @@ public class BloodContainer extends ConfigurableDamageIndicatedItemFluidContaine
     
     @Override
     public boolean hasEffect(ItemStack itemStack){
-        return isContainerActivated(itemStack);
+        return ItemHelpers.isActivated(itemStack);
     }
     
     @SuppressWarnings({ "rawtypes", "unchecked" })
@@ -112,7 +112,7 @@ public class BloodContainer extends ConfigurableDamageIndicatedItemFluidContaine
         super.addInformation(itemStack, entityPlayer, list, par4);
         list.add(IInformationProvider.INFO_PREFIX+"Shift + Right click to toggle auto-supply.");
         String autoSupply = EnumChatFormatting.RESET+"Disabled";
-        if(isContainerActivated(itemStack)) {
+        if(ItemHelpers.isActivated(itemStack)) {
             autoSupply = EnumChatFormatting.GREEN + "Enabled";
         }
         list.add(EnumChatFormatting.BOLD + "Auto-supply: " + autoSupply);
@@ -120,7 +120,7 @@ public class BloodContainer extends ConfigurableDamageIndicatedItemFluidContaine
     
     @Override
     public void onUpdate(ItemStack itemStack, World world, Entity entity, int par4, boolean par5) {
-        if(entity instanceof EntityPlayer && !world.isRemote && isContainerActivated(itemStack)) {
+        if(entity instanceof EntityPlayer && !world.isRemote && ItemHelpers.isActivated(itemStack)) {
             FluidStack tickFluid = this.getFluid(itemStack);
             if(tickFluid != null && tickFluid.amount > 0) {
                 EntityPlayer player = (EntityPlayer) entity;
@@ -142,28 +142,6 @@ public class BloodContainer extends ConfigurableDamageIndicatedItemFluidContaine
             }
         }
         super.onUpdate(itemStack, world, entity, par4, par5);
-    }
-    
-    /**
-     * Check if the given item is an activated container.
-     * @param itemStack The item to check
-     * @return If it is an active container.
-     */
-    public static boolean isContainerActivated(ItemStack itemStack) {
-        return itemStack != null && itemStack.getTagCompound() != null && itemStack.getTagCompound().getBoolean("enabled");
-    }
-    
-    /**
-     * Toggle activation for the given item.
-     * @param itemStack The item to toggle.
-     */
-    public static void toggleContainerActivation(ItemStack itemStack) {
-        NBTTagCompound tag = itemStack.getTagCompound();
-        if(tag == null) {
-            tag = new NBTTagCompound();
-            itemStack.setTagCompound(tag);
-        }
-        tag.setBoolean("enabled", !isContainerActivated(itemStack));
     }
 
 }
