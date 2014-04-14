ad1863ea7fd62dcbf8909c6680bcc7f7d6f32e40
ganymedes01 <foka_12@hotmail.com>
2014-04-14 13:41:48 -0300

    Fix ExcrementPile crash
    By using that method the even creates a fake player holding the passed ItemStack, it will prevent null pointers from being thrown by other mods who assume the player is always true (which is reasonable)

--- common/evilcraft/blocks/ExcrementPile.java
+++ common/evilcraft/blocks/ExcrementPile.java
@@ -144,9 +144,9 @@ public class ExcrementPile extends ConfigurableBlock implements IInformationProv
                             if(blockIDBelow == Block.dirt.blockID) {
                                 world.setBlock(xr, y - 1, zr, Block.grass.blockID);
                             } else if(blockIDBelow == Block.grass.blockID) {
-                                ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1), world, xr, y - 1, zr, null);
+                                ItemDye.func_96604_a(new ItemStack(Item.dyePowder, 1, 15), world, xr, y - 1, zr)
                             }
-                            ItemDye.applyBonemeal(new ItemStack(Item.dyePowder, 1), world, xr, y, zr, null);
+                            ItemDye.func_96604_a(new ItemStack(Item.dyePowder, 1, 15), world, xr, y, zr)
                         }
                     }
                 }
