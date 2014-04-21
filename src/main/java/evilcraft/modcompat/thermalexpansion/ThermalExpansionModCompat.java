2e071f44ca635b30a05689747b14380154d870e1
Ruben Taelman <rubensworks@gmail.com>
2014-04-21 12:10:59 +0200

    Fixed TE mod compact compilation errors. The API interaction is not expected to work yet, since TE is not yet released for 1.7.2.

--- src/main/java/evilcraft/modcompat/thermalexpansion/ThermalExpansionModCompat.java
+++ src/main/java/evilcraft/modcompat/thermalexpansion/ThermalExpansionModCompat.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -68,8 +69,8 @@ public class ThermalExpansionModCompat implements IModCompat {
         if(Configs.isEnabled(UndeadLogConfig.class) && Configs.isEnabled(UndeadPlankConfig.class)) {
             NBTTagCompound sawmillUndeadWood = new NBTTagCompound();
             sawmillUndeadWood.setInteger("energy", 2000);
-            sawmillUndeadWood.setCompoundTag("input", new NBTTagCompound());
-            sawmillUndeadWood.setCompoundTag("primaryOutput", new NBTTagCompound());
+            sawmillUndeadWood.setTag("input", new NBTTagCompound());
+            sawmillUndeadWood.setTag("primaryOutput", new NBTTagCompound());
     
             new ItemStack(UndeadLog.getInstance()).writeToNBT(sawmillUndeadWood.getCompoundTag("input"));
             new ItemStack(UndeadPlank.getInstance(), 6).writeToNBT(sawmillUndeadWood.getCompoundTag("primaryOutput"));
@@ -80,8 +81,8 @@ public class ThermalExpansionModCompat implements IModCompat {
         if(Configs.isEnabled(DarkOreConfig.class) && Configs.isEnabled(DarkGemConfig.class)) {
             NBTTagCompound pulverizerDarkOre = new NBTTagCompound();
             pulverizerDarkOre.setInteger("energy", 2000);
-            pulverizerDarkOre.setCompoundTag("input", new NBTTagCompound());
-            pulverizerDarkOre.setCompoundTag("primaryOutput", new NBTTagCompound());
+            pulverizerDarkOre.setTag("input", new NBTTagCompound());
+            pulverizerDarkOre.setTag("primaryOutput", new NBTTagCompound());
     
             new ItemStack(DarkOre.getInstance()).writeToNBT(pulverizerDarkOre.getCompoundTag("input"));
             new ItemStack(DarkGem.getInstance(), 2).writeToNBT(pulverizerDarkOre.getCompoundTag("primaryOutput"));
@@ -93,8 +94,8 @@ public class ThermalExpansionModCompat implements IModCompat {
         for(ItemStack materialPoisonous : materialPoisonousList) {
             NBTTagCompound cruciblePoison = new NBTTagCompound();
             cruciblePoison.setInteger("energy", 2000);
-            cruciblePoison.setCompoundTag("input", new NBTTagCompound());
-            cruciblePoison.setCompoundTag("output", new NBTTagCompound());
+            cruciblePoison.setTag("input", new NBTTagCompound());
+            cruciblePoison.setTag("output", new NBTTagCompound());
 
             materialPoisonous.writeToNBT(cruciblePoison.getCompoundTag("input"));
             new FluidStack(Poison.getInstance(), 250).writeToNBT(cruciblePoison.getCompoundTag("output"));
@@ -107,8 +108,8 @@ public class ThermalExpansionModCompat implements IModCompat {
                 ItemStack materialPoisonous = new ItemStack(BloodStainedBlock.getInstance(), 1, i);
                 NBTTagCompound crucibleBlood = new NBTTagCompound();
                 crucibleBlood.setInteger("energy", 2000);
-                crucibleBlood.setCompoundTag("input", new NBTTagCompound());
-                crucibleBlood.setCompoundTag("output", new NBTTagCompound());
+                crucibleBlood.setTag("input", new NBTTagCompound());
+                crucibleBlood.setTag("output", new NBTTagCompound());
     
                 materialPoisonous.writeToNBT(crucibleBlood.getCompoundTag("input"));
                 new FluidStack(Blood.getInstance(), BloodExtractorConfig.maxMB).writeToNBT(crucibleBlood.getCompoundTag("output"));
@@ -122,9 +123,9 @@ public class ThermalExpansionModCompat implements IModCompat {
             for(Entry<CustomRecipe, CustomRecipeResult> entry : bloodInfuseRecipes.entrySet()) {
                 NBTTagCompound bloodInfuse = new NBTTagCompound();
                 bloodInfuse.setInteger("energy", entry.getKey().getDuration() * 100);
-                bloodInfuse.setCompoundTag("input", new NBTTagCompound());
-                bloodInfuse.setCompoundTag("output", new NBTTagCompound());
-                bloodInfuse.setCompoundTag("fluid", new NBTTagCompound());
+                bloodInfuse.setTag("input", new NBTTagCompound());
+                bloodInfuse.setTag("output", new NBTTagCompound());
+                bloodInfuse.setTag("fluid", new NBTTagCompound());
     
                 entry.getKey().getItemStack().writeToNBT(bloodInfuse.getCompoundTag("input"));
                 entry.getValue().getResult().writeToNBT(bloodInfuse.getCompoundTag("output"));
@@ -140,12 +141,12 @@ public class ThermalExpansionModCompat implements IModCompat {
         for(Entry<Item, FluidStack> entry : Recipes.BUCKETS.entrySet()) {
             NBTTagCompound fill = new NBTTagCompound();
             fill.setInteger("energy", 2000);
-            fill.setCompoundTag("input", new NBTTagCompound());
-            fill.setCompoundTag("output", new NBTTagCompound());
-            fill.setCompoundTag("fluid", new NBTTagCompound());
+            fill.setTag("input", new NBTTagCompound());
+            fill.setTag("output", new NBTTagCompound());
+            fill.setTag("fluid", new NBTTagCompound());
 
             new ItemStack(entry.getKey()).writeToNBT(fill.getCompoundTag("input"));
-            new ItemStack(Item.bucketEmpty).writeToNBT(fill.getCompoundTag("output"));
+            new ItemStack(Items.bucket).writeToNBT(fill.getCompoundTag("output"));
             fill.setBoolean("reversible", true);
             entry.getValue().copy().writeToNBT(fill.getCompoundTag("fluid"));
             FMLInterModComms.sendMessage(TE, "TransposerFillRecipe", fill);
