f63bd00ec290940c04189c47ce4aa6aab6e2078c
Ruben Taelman <rubensworks@gmail.com>
2014-08-03 12:46:55 +0200

    Remove invalid throws exception in the FakeSaveHandler.

--- src/main/java/evilcraft/api/world/FakeSaveHandler.java
+++ src/main/java/evilcraft/api/world/FakeSaveHandler.java
@@ -3,7 +3,6 @@ package evilcraft.api.world;
 import java.io.File;
 
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.world.MinecraftException;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.storage.IChunkLoader;
 import net.minecraft.world.storage.IPlayerFileData;
@@ -23,7 +22,7 @@ public class FakeSaveHandler implements ISaveHandler {
 	}
 
 	@Override
-	public void checkSessionLock() throws MinecraftException {
+	public void checkSessionLock() {
 		
 	}
 
