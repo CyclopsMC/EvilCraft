cfb637a27c90237a036930356d65c937e542e74a
Ruben Taelman <rubensworks@gmail.com>
2014-08-09 15:06:12 +0200

    Update the build process for releases.

--- release.sh
+++ release.sh
@@ -10,7 +10,9 @@ fi
 # Java 1.8 does not seem to work well yet...
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
 
-BUILD_NUMBER=$(cat build_number.properties)
-export BUILD_NUMBER
+#BUILD_NUMBER=$(cat build_number.properties)
+#export BUILD_NUMBER
+RELEASE=1
+export RELEASE
 
 gradle build
