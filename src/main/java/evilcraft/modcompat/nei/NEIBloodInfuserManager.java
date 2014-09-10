CyclopsMC/EvilCraft

commit 8f18dd436cabdc1cb5b22476a17150b4ee5626e1

Replace all references to old recipe registration
Deleted all the old CustomRecipe and RecipeRegistry classes and
refactored existing code to use the new Recipe Registration system.

src/main/java/evilcraft/modcompat/nei/NEIBloodInfuserManager.java
@@ -7,6 +7,10 @@ import java.awt.Rectangle;
 import java.util.LinkedList;
 import java.util.List;
 
+import evilcraft.api.recipes.DurationRecipeProperties;
+import evilcraft.api.recipes.IRecipe;
+import evilcraft.api.recipes.ItemAndFluidStackRecipeComponent;
+import evilcraft.api.recipes.ItemStackRecipeComponent;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
@@ -22,9 +26,6 @@ import codechicken.nei.NEIServerUtils;
 import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import evilcraft.Reference;
-import evilcraft.api.recipes.CustomRecipe;
-import evilcraft.api.recipes.CustomRecipeRegistry;
-import evilcraft.api.recipes.CustomRecipeResult;
 import evilcraft.blocks.BloodInfuser;
 import evilcraft.blocks.BloodInfuserConfig;
 import evilcraft.entities.tileentities.TileBloodInfuser;
@@ -68,6 +69,11 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
         private PositionedStack output;
         private FluidStack fluidStack;
         private int duration;
+
+        public CachedBloodInfuserRecipe(IRecipe<ItemAndFluidStackRecipeComponent, ItemStackRecipeComponent, DurationRecipeProperties> recipe) {
+            this(recipe.getInput().getItemStack(), recipe.getOutput().getItemStack(),
+                    recipe.getInput().getFluidStack(), recipe.getProperties().getDuration());
+        }
         
         public CachedBloodInfuserRecipe(ItemStack inputStack, ItemStack outputStack, FluidStack fluidStack, int duration) {
             this.input = 
@@ -149,16 +155,10 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
     
     private List<CachedBloodInfuserRecipe> getRecipes() {
         List<CachedBloodInfuserRecipe> recipes = new LinkedList<CachedBloodInfuserRecipe>();
-        for(CustomRecipeResult recipeResult : CustomRecipeRegistry.getRecipesForFactory(factoryBlock).values()) {
-        	recipes.add(
-                    new CachedBloodInfuserRecipe(
-                            recipeResult.getRecipe().getItemStack(),
-                            recipeResult.getResult(),
-                            recipeResult.getRecipe().getFluidStack(),
-                            recipeResult.getRecipe().getDuration()
-                            )
-                    );
-        }
+
+        for (IRecipe recipe : BloodInfuser.getInstance().getRecipeRegistry().allRecipes())
+            recipes.add(new CachedBloodInfuserRecipe(recipe));
+
         return recipes;
     }
     
@@ -180,37 +180,24 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
     
     @Override
     public void loadCraftingRecipes(ItemStack result) {
-        CustomRecipe recipe = CustomRecipeRegistry.get(result);
-        if(recipe != null && recipe.getFactory() == factoryBlock) {
-            arecipes.add(
-                    new CachedBloodInfuserRecipe(
-                            recipe.getItemStack(),
-                            result,
-                            recipe.getFluidStack(),
-                            recipe.getDuration()
-                            )
-                    );
+        List<IRecipe<ItemAndFluidStackRecipeComponent, ItemStackRecipeComponent, DurationRecipeProperties>> recipes =
+                BloodInfuser.getInstance().getRecipeRegistry().findRecipesByOutput(new ItemStackRecipeComponent(result));
+
+        if(!recipes.isEmpty()) {
+            arecipes.add(new CachedBloodInfuserRecipe(recipes.get(0)));
         }
     }
     
     @Override
     public void loadUsageRecipes(ItemStack ingredient) {
-        CustomRecipe customRecipeKey =
-                new CustomRecipe(
-                    ingredient,
-                    new FluidStack(TileBloodInfuser.ACCEPTED_FLUID, TileBloodInfuser.LIQUID_PER_SLOT),
-                    factoryBlock
-                );
-        CustomRecipeResult recipeResult = CustomRecipeRegistry.get(customRecipeKey);
-        if(recipeResult != null) {
-            arecipes.add(
-                    new CachedBloodInfuserRecipe(
-                            recipeResult.getRecipe().getItemStack(),
-                            recipeResult.getResult(),
-                            recipeResult.getRecipe().getFluidStack(),
-                            recipeResult.getRecipe().getDuration()
-                            )
-                    );
+        IRecipe recipe = BloodInfuser.getInstance().getRecipeRegistry().findRecipeByInput(
+                new ItemAndFluidStackRecipeComponent(
+                        ingredient,
+                        new FluidStack(TileBloodInfuser.ACCEPTED_FLUID, TileBloodInfuser.LIQUID_PER_SLOT))
+        );
+
+        if(recipe != null) {
+            arecipes.add(new CachedBloodInfuserRecipe(recipe));
         }
     }
 

src/main/java/evilcraft/modcompat/nei/NEIEnvironmentalAccumulatorManager.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import evilcraft.api.recipes.*;
 import net.minecraft.item.ItemStack;
 
 import org.lwjgl.opengl.GL11;
@@ -16,11 +17,6 @@ import org.lwjgl.opengl.GL11;
 import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import evilcraft.Reference;
-import evilcraft.api.recipes.CustomRecipe;
-import evilcraft.api.recipes.CustomRecipeRegistry;
-import evilcraft.api.recipes.CustomRecipeResult;
-import evilcraft.api.recipes.EnvironmentalAccumulatorRecipe;
-import evilcraft.api.recipes.EnvironmentalAccumulatorResult;
 import evilcraft.api.weather.WeatherType;
 import evilcraft.blocks.EnvironmentalAccumulator;
 import evilcraft.blocks.EnvironmentalAccumulatorConfig;
@@ -54,6 +50,15 @@ public class NEIEnvironmentalAccumulatorManager extends TemplateRecipeHandler {
         private PositionedStack outputStack;
         private WeatherType outputWeather;
         private int duration;
+
+        public CachedEnvironmentalAccumulatorRecipe(
+                EnvironmentalAccumulatorRecipeComponent input,
+                EnvironmentalAccumulatorRecipeComponent output,
+                EnvironmentalAccumulatorRecipeProperties properties) {
+            this(input.getItemStack(), input.getWeatherType(),
+                 output.getItemStack(), output.getWeatherType(),
+                 properties.getDuration());
+        }
         
         public CachedEnvironmentalAccumulatorRecipe(
                 ItemStack inputStack, WeatherType inputWeather,
@@ -181,18 +186,12 @@ public class NEIEnvironmentalAccumulatorManager extends TemplateRecipeHandler {
     
     private List<CachedEnvironmentalAccumulatorRecipe> getRecipes() {
         List<CachedEnvironmentalAccumulatorRecipe> recipes = new LinkedList<CachedEnvironmentalAccumulatorRecipe>();
-        for(CustomRecipeResult recipeResult : CustomRecipeRegistry.getRecipesForFactory(EnvironmentalAccumulator.getInstance()).values()) {
-            EnvironmentalAccumulatorRecipe eaRecipe = (EnvironmentalAccumulatorRecipe) recipeResult.getRecipe();
-            EnvironmentalAccumulatorResult eaResult = (EnvironmentalAccumulatorResult) recipeResult;
-            recipes.add(
-                    new CachedEnvironmentalAccumulatorRecipe(
-                            recipeResult.getRecipe().getItemStack(),
-                            eaRecipe.getWeatherType(),
-                            recipeResult.getResult(),
-                            eaResult.getWeatherResult(),
-                            recipeResult.getRecipe().getDuration()
-                            )
-                    );
+        for (IRecipe recipe : EnvironmentalAccumulator.getInstance().getRecipeRegistry().allRecipes()) {
+            EnvironmentalAccumulatorRecipeComponent input = (EnvironmentalAccumulatorRecipeComponent)recipe.getInput();
+            EnvironmentalAccumulatorRecipeComponent output = (EnvironmentalAccumulatorRecipeComponent)recipe.getOutput();
+            EnvironmentalAccumulatorRecipeProperties props = (EnvironmentalAccumulatorRecipeProperties)recipe.getProperties();
+
+            recipes.add(new CachedEnvironmentalAccumulatorRecipe(input, output, props));
         }
         return recipes;
     }
@@ -214,44 +213,29 @@ public class NEIEnvironmentalAccumulatorManager extends TemplateRecipeHandler {
     }
     
     @Override
-    public void loadCraftingRecipes(ItemStack result) {
-        CustomRecipe recipe = CustomRecipeRegistry.get(result);
-        if(recipe != null && recipe.getFactory() == EnvironmentalAccumulator.getInstance()) {
-            EnvironmentalAccumulatorRecipe eaRecipe = (EnvironmentalAccumulatorRecipe)recipe;
-            EnvironmentalAccumulatorResult eaResult = (EnvironmentalAccumulatorResult) CustomRecipeRegistry.get(eaRecipe);
-            
-            arecipes.add(
-                    new CachedEnvironmentalAccumulatorRecipe(
-                            eaRecipe.getItemStack(),
-                            eaRecipe.getWeatherType(),
-                            eaResult.getItemResult(),
-                            eaResult.getWeatherResult(),
-                            eaRecipe.getDuration()
-                            )
-                    );
+    public void loadCraftingRecipes(final ItemStack result) {
+        IRecipe recipe = EnvironmentalAccumulator.getInstance().getRecipeRegistry().findRecipeByOutput(
+                new EnvironmentalAccumulatorRecipeComponent(result, WeatherType.ANY)
+        );
+
+        if (recipe != null) {
+            arecipes.add(new CachedEnvironmentalAccumulatorRecipe(
+                    (EnvironmentalAccumulatorRecipeComponent) recipe.getInput(),
+                    (EnvironmentalAccumulatorRecipeComponent) recipe.getOutput(),
+                    (EnvironmentalAccumulatorRecipeProperties) recipe.getProperties()
+            ));
         }
     }
     
     @Override
-    public void loadUsageRecipes(ItemStack ingredient) {
-        CustomRecipe customRecipeKey =
-                new EnvironmentalAccumulatorRecipe(
-                        null,
-                        ingredient
-                );
-        EnvironmentalAccumulatorResult eaResult = (EnvironmentalAccumulatorResult) CustomRecipeRegistry.get(customRecipeKey);
-        if(eaResult != null) {
-            EnvironmentalAccumulatorRecipe eaRecipe = (EnvironmentalAccumulatorRecipe) CustomRecipeRegistry.get(eaResult.getItemResult());
-            
-            arecipes.add(
-                    new CachedEnvironmentalAccumulatorRecipe(
-                            eaRecipe.getItemStack(),
-                            eaRecipe.getWeatherType(),
-                            eaResult.getItemResult(),
-                            eaResult.getWeatherResult(),
-                            eaRecipe.getDuration()
-                            )
-                    );
+    public void loadUsageRecipes(final ItemStack ingredient) {
+        for (IRecipe<EnvironmentalAccumulatorRecipeComponent, EnvironmentalAccumulatorRecipeComponent, EnvironmentalAccumulatorRecipeProperties> recipe :
+                EnvironmentalAccumulator.getInstance().getRecipeRegistry().findRecipesByInput(new EnvironmentalAccumulatorRecipeComponent(ingredient, WeatherType.ANY))) {
+            arecipes.add(new CachedEnvironmentalAccumulatorRecipe(
+                    recipe.getInput(),
+                    recipe.getOutput(),
+                    recipe.getProperties()
+            ));
         }
     }
     

src/main/java/evilcraft/entities/tileentities/TileBloodInfuser.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import evilcraft.api.recipes.IRecipe;
+import evilcraft.api.recipes.ItemAndFluidStackRecipeComponent;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBucket;
@@ -21,9 +23,6 @@ import evilcraft.api.fluids.BloodFluidConverter;
 import evilcraft.api.fluids.ImplicitFluidConversionTank;
 import evilcraft.api.fluids.SingleUseTank;
 import evilcraft.api.gui.slot.SlotFluidContainer;
-import evilcraft.api.recipes.CustomRecipe;
-import evilcraft.api.recipes.CustomRecipeRegistry;
-import evilcraft.api.recipes.CustomRecipeResult;
 import evilcraft.blocks.BloodInfuser;
 import evilcraft.entities.tileentities.tickaction.EmptyFluidContainerInTankTickAction;
 import evilcraft.entities.tileentities.tickaction.EmptyItemBucketInTankTickAction;
@@ -152,9 +151,9 @@ public class TileBloodInfuser extends WorkingTileEntity<TileBloodInfuser> {
         }
         
         // Valid custom recipe
-        CustomRecipe customRecipeKey = new CustomRecipe(itemStack, getTank().getFluid(), BloodInfuser.getInstance());
-        CustomRecipeResult result = CustomRecipeRegistry.get(customRecipeKey);
-        if(result != null)
+        ItemAndFluidStackRecipeComponent recipeInput = new ItemAndFluidStackRecipeComponent(itemStack, getTank().getFluid());
+        IRecipe recipe = BloodInfuser.getInstance().getRecipeRegistry().findRecipeByInput(recipeInput);
+        if(recipe != null)
             return true;
         
         // In all other cases: false

src/main/java/evilcraft/entities/tileentities/environmentalaccumulator/IEAProcessingFinishedEffect.java
@@ -1,7 +1,6 @@
 package evilcraft.entities.tileentities.environmentalaccumulator;
 
-import evilcraft.api.recipes.EnvironmentalAccumulatorRecipe;
-import evilcraft.api.recipes.EnvironmentalAccumulatorResult;
+import evilcraft.api.recipes.IRecipe;
 import evilcraft.blocks.EnvironmentalAccumulator;
 import evilcraft.entities.tileentities.TileEnvironmentalAccumulator;
 
@@ -21,7 +20,6 @@ public interface IEAProcessingFinishedEffect {
      * 
      * @param tile The tile entity that called this function.
      * @param recipe The recipe that caused this effect to be executed.
-     * @param result The result belong to the given recipe.
      */
-    public void executeEffect(TileEnvironmentalAccumulator tile, EnvironmentalAccumulatorRecipe recipe, EnvironmentalAccumulatorResult result);
+    public void executeEffect(TileEnvironmentalAccumulator tile, IRecipe recipe);
 }

commit cca8bf795266af6a5bc2e46afd0f85d8bff87514

Add missing generic types caused by 8f18dd4.

src/main/java/evilcraft/modcompat/nei/NEIBloodInfuserManager.java
@@ -7,7 +7,6 @@ import java.awt.Rectangle;
 import java.util.LinkedList;
 import java.util.List;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.Tessellator;
@@ -59,8 +58,6 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
     private final int progressHeight = GuiBloodInfuser.PROGRESSHEIGHT;
     
     private float zLevel = 200.0F;
-    
-    private Block factoryBlock = BloodInfuser.getInstance();
     
     private class CachedBloodInfuserRecipe extends CachedRecipe {
         
@@ -156,7 +153,8 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
     private List<CachedBloodInfuserRecipe> getRecipes() {
         List<CachedBloodInfuserRecipe> recipes = new LinkedList<CachedBloodInfuserRecipe>();
 
-        for (IRecipe recipe : BloodInfuser.getInstance().getRecipeRegistry().allRecipes())
+        for (IRecipe<ItemAndFluidStackRecipeComponent, ItemStackRecipeComponent, DurationRecipeProperties> recipe : 
+        	BloodInfuser.getInstance().getRecipeRegistry().allRecipes())
             recipes.add(new CachedBloodInfuserRecipe(recipe));
 
         return recipes;
@@ -190,7 +188,8 @@ public class NEIBloodInfuserManager extends TemplateRecipeHandler {
     
     @Override
     public void loadUsageRecipes(ItemStack ingredient) {
-        IRecipe recipe = BloodInfuser.getInstance().getRecipeRegistry().findRecipeByInput(
+        IRecipe<ItemAndFluidStackRecipeComponent, ItemStackRecipeComponent, DurationRecipeProperties> recipe = BloodInfuser
+        		.getInstance().getRecipeRegistry().findRecipeByInput(
                 new ItemAndFluidStackRecipeComponent(
                         ingredient,
                         new FluidStack(TileBloodInfuser.ACCEPTED_FLUID, TileBloodInfuser.LIQUID_PER_SLOT))

src/main/java/evilcraft/modcompat/nei/NEIEnvironmentalAccumulatorManager.java
@@ -188,7 +188,9 @@ public class NEIEnvironmentalAccumulatorManager extends TemplateRecipeHandler {
     
     private List<CachedEnvironmentalAccumulatorRecipe> getRecipes() {
         List<CachedEnvironmentalAccumulatorRecipe> recipes = new LinkedList<CachedEnvironmentalAccumulatorRecipe>();
-        for (IRecipe recipe : EnvironmentalAccumulator.getInstance().getRecipeRegistry().allRecipes()) {
+        for (IRecipe<EnvironmentalAccumulatorRecipeComponent, EnvironmentalAccumulatorRecipeComponent, 
+        		EnvironmentalAccumulatorRecipeProperties> recipe : EnvironmentalAccumulator.getInstance().getRecipeRegistry()
+        		.allRecipes()) {
             EnvironmentalAccumulatorRecipeComponent input = (EnvironmentalAccumulatorRecipeComponent)recipe.getInput();
             EnvironmentalAccumulatorRecipeComponent output = (EnvironmentalAccumulatorRecipeComponent)recipe.getOutput();
             EnvironmentalAccumulatorRecipeProperties props = (EnvironmentalAccumulatorRecipeProperties)recipe.getProperties();
@@ -216,7 +218,9 @@ public class NEIEnvironmentalAccumulatorManager extends TemplateRecipeHandler {
     
     @Override
     public void loadCraftingRecipes(final ItemStack result) {
-        IRecipe recipe = EnvironmentalAccumulator.getInstance().getRecipeRegistry().findRecipeByOutput(
+        IRecipe<EnvironmentalAccumulatorRecipeComponent, EnvironmentalAccumulatorRecipeComponent, 
+        	EnvironmentalAccumulatorRecipeProperties> recipe = EnvironmentalAccumulator.getInstance().getRecipeRegistry()
+        	.findRecipeByOutput(
                 new EnvironmentalAccumulatorRecipeComponent(result, WeatherType.ANY)
         );
 

src/main/java/evilcraft/tileentity/TileBloodInfuser.java
@@ -20,7 +20,9 @@ import evilcraft.core.fluid.BloodFluidConverter;
 import evilcraft.core.fluid.ImplicitFluidConversionTank;
 import evilcraft.core.fluid.SingleUseTank;
 import evilcraft.core.inventory.slot.SlotFluidContainer;
+import evilcraft.core.recipe.custom.DurationRecipeProperties;
 import evilcraft.core.recipe.custom.ItemAndFluidStackRecipeComponent;
+import evilcraft.core.recipe.custom.ItemStackRecipeComponent;
 import evilcraft.core.tileentity.WorkingTileEntity;
 import evilcraft.core.tileentity.tickaction.ITickAction;
 import evilcraft.core.tileentity.tickaction.TickComponent;
@@ -152,7 +154,8 @@ public class TileBloodInfuser extends WorkingTileEntity<TileBloodInfuser> {
         
         // Valid custom recipe
         ItemAndFluidStackRecipeComponent recipeInput = new ItemAndFluidStackRecipeComponent(itemStack, getTank().getFluid());
-        IRecipe recipe = BloodInfuser.getInstance().getRecipeRegistry().findRecipeByInput(recipeInput);
+        IRecipe<ItemAndFluidStackRecipeComponent, ItemStackRecipeComponent, DurationRecipeProperties> recipe = 
+        		BloodInfuser.getInstance().getRecipeRegistry().findRecipeByInput(recipeInput);
         if(recipe != null)
             return true;
         

src/main/java/evilcraft/tileentity/environmentalaccumulator/IEAProcessingFinishedEffect.java
@@ -2,6 +2,8 @@ package evilcraft.tileentity.environmentalaccumulator;
 
 import evilcraft.api.recipes.custom.IRecipe;
 import evilcraft.block.EnvironmentalAccumulator;
+import evilcraft.core.recipe.custom.EnvironmentalAccumulatorRecipeComponent;
+import evilcraft.core.recipe.custom.EnvironmentalAccumulatorRecipeProperties;
 import evilcraft.tileentity.TileEnvironmentalAccumulator;
 
 /**
@@ -21,5 +23,6 @@ public interface IEAProcessingFinishedEffect {
      * @param tile The tile entity that called this function.
      * @param recipe The recipe that caused this effect to be executed.
      */
-    public void executeEffect(TileEnvironmentalAccumulator tile, IRecipe recipe);
+    public void executeEffect(TileEnvironmentalAccumulator tile, IRecipe<EnvironmentalAccumulatorRecipeComponent, 
+    		EnvironmentalAccumulatorRecipeComponent, EnvironmentalAccumulatorRecipeProperties> recipe);
 }