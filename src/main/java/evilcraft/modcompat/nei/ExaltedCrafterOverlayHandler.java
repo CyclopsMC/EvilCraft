273b4c4cf496701124c1466fb3ad37ffc30c5d23
Ruben Taelman <rubensworks@gmail.com>
2014-11-05 21:30:39 +0100

    Re-add custom moveFrom NEI support for Exalted Crafter. #157

--- src/main/java/evilcraft/modcompat/nei/ExaltedCrafterOverlayHandler.java
+++ src/main/java/evilcraft/modcompat/nei/ExaltedCrafterOverlayHandler.java
@@ -1,19 +1,23 @@
 package evilcraft.modcompat.nei;
 
 import codechicken.nei.recipe.DefaultOverlayHandler;
+import evilcraft.core.inventory.NBTSimpleInventory;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.InventoryEnderChest;
+import net.minecraft.inventory.Slot;
 
 /**
  * An overlay handler for the Exalted Crafter.
  * @author rubensworks
- * TODO: re-implement the canMoveFrom method once NEI is a bit more stable...
  */
-@Deprecated
 public class ExaltedCrafterOverlayHandler extends DefaultOverlayHandler {
 
-	/*@Override
+	@Override
 	public boolean canMoveFrom(Slot slot, GuiContainer gui) {
         return slot.inventory instanceof InventoryPlayer
-        		|| slot.inventory instanceof InventoryEnderChest;
-    }*/
+        		|| slot.inventory instanceof InventoryEnderChest
+                || slot.inventory instanceof NBTSimpleInventory;
+    }
 	
 }
