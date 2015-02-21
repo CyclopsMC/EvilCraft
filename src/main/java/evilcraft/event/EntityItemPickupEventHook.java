f6da97fa17e7848d4c154525e6b21bd11417cb6b
Ruben Taelman <rubensworks@gmail.com>
2015-02-21 21:26:00 +0100

    Fix Origins of Darkness not obtainable in SMP.

--- src/main/java/evilcraft/event/EntityItemPickupEventHook.java
+++ src/main/java/evilcraft/event/EntityItemPickupEventHook.java
@@ -2,8 +2,6 @@ package evilcraft.event;
 
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import evilcraft.Achievements;
 import evilcraft.item.OriginsOfDarkness;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -19,7 +17,6 @@ public class EntityItemPickupEventHook {
      * When a player tick event is received.
      * @param event The received event.
      */
-	@SideOnly(Side.CLIENT)
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void onTick(EntityItemPickupEvent event) {
         originsOfDarknessObtain(event);
