c7d582270521f8c16db2c7e0acd504c83c3d7a82
Ruben Taelman <rubensworks@gmail.com>
2015-02-22 21:18:30 +0100

    Fix Spirit Furnace not clickable anymore. That is because of the refactoring in 88a62d8c2cf432862c23b460540f5e46091481c3. And that, dear children, is why refactoring is do dangerous!

--- src/main/java/evilcraft/block/SpiritFurnace.java
+++ src/main/java/evilcraft/block/SpiritFurnace.java
@@ -71,7 +71,7 @@ public class SpiritFurnace extends ConfigurableBlockContainerGuiTankInfo impleme
     
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer entityplayer, int par6, float par7, float par8, float par9) {
-    	return !TileSpiritFurnace.canWork(world, new Location(x, y, z)) &&
+        return !TileSpiritFurnace.canWork(world, new Location(x, y, z)) ||
                 super.onBlockActivated(world, x, y, z, entityplayer, par6, par7, par8, par9);
     }
     
