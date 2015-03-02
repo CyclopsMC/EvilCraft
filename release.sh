5a8fd285032f9edd922f52228c0defd0dc759c79
Ruben Taelman <rubensworks@gmail.com>
2015-03-02 15:53:47 +0100

    Automate github releases and tagging.

--- release.sh
+++ release.sh
@@ -9,3 +9,12 @@ export RELEASE
 ./gradlew build
 ./gradlew uploadArchives
 ./gradlew curse
+
+# Github release
+minecraft_version=$(grep minecraft_version= build.properties | sed s/minecraft_version=//)
+tag=$(grep mod_version= build.properties | sed s/mod_version=//)
+name="EvilCraft-"$minecraft_version"-"$tag
+changelog=$(cat changelog.txt)
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "%s","draft": false,"prerelease": false}' $tag $name "$changelog")
+ACCESS_TOKEN=$(grep github_token= gradle.properties | sed s/github_token=//)
+curl --data "$API_JSON" https://api.github.com/repos/rubensworks/EvilCraft/releases?access_token=$ACCESS_TOKEN
