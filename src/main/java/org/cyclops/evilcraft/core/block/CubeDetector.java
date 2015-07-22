cde1b40d3e64fc8ed4141e062f50413bd9628942
Ruben Taelman <rubensworks@gmail.com>
2015-07-22 11:17:48 +0200

    Fix minimum size requirement of the Spirit Furnace

--- src/main/java/org/cyclops/evilcraft/core/block/CubeDetector.java
+++ src/main/java/org/cyclops/evilcraft/core/block/CubeDetector.java
@@ -379,7 +379,7 @@ public class CubeDetector {
 		// we skip this step because we always want to be able to invalidate existing structures.
 		// TODO: the 'valid' condition might be impossible to have influence if all structure
 		// handling goes according to plan.
-		if(size.compareTo(getMinimumSize()) < 0 && valid) {
+		if(compareVec3i(size, getMinimumSize()) < 0 && valid) {
 			// System.out.println("too small");
 			return LocationHelpers.copyLocation(NULL_SIZE);
 		}
@@ -388,6 +388,27 @@ public class CubeDetector {
         }
 		return size;
 	}
+
+	protected int compareVec3i(Vec3i vec1, Vec3i vec2) {
+        int i = 0;
+        boolean validBuffer = false;
+        int buffer = Integer.MAX_VALUE; // This is used to store the minimum compared value > 0
+        int[] v1 = LocationHelpers.toArray(vec1);
+        int[] v2 = LocationHelpers.toArray(vec2);
+        while(i < 3) {
+            if(v1[i] != v2[i]) {
+                int comp =  v1[i] - v2[i];
+                if(comp < 0) {
+                    return comp;
+                } else {
+                    validBuffer = true;
+                    buffer = Math.min(buffer, comp);
+                }
+            }
+            i++;
+        }
+        return validBuffer ? 0 : buffer;
+    }
 	
 	/**
 	 * Listener for detections.
