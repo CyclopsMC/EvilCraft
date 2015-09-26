3c3d2622bc53bdd3016397d878c9d8adc6f4d679
Ruben Taelman <rubensworks@gmail.com>
2015-09-26 18:59:54 +0200

    Dark Tank in auto-extract will also fill fluid container items below it

--- src/main/java/evilcraft/tileentity/TileDarkTank.java
+++ src/main/java/evilcraft/tileentity/TileDarkTank.java
@@ -3,12 +3,18 @@ package evilcraft.tileentity;
 import evilcraft.GeneralConfig;
 import evilcraft.block.DarkTank;
 import evilcraft.core.tileentity.TankInventoryTileEntity;
+import net.minecraft.command.IEntitySelector;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.common.util.ForgeDirection;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.IFluidContainerItem;
 import net.minecraftforge.fluids.IFluidHandler;
+
+import java.util.List;
 
 /**
  * Tile Entity for the dark tank.
@@ -60,14 +66,38 @@ public class TileDarkTank extends TankInventoryTileEntity {
 	protected void updateTileEntity() {
 		if(!getTank().isEmpty() && shouldAutoDrain() && !getWorldObj().isRemote) {
 			ForgeDirection down = ForgeDirection.DOWN;
-			TileEntity tile = worldObj.getTileEntity(xCoord + down.offsetX, yCoord + down.offsetY, zCoord + down.offsetZ);
-			if(tile instanceof IFluidHandler) {
-				IFluidHandler handler = (IFluidHandler) tile;
-				FluidStack fluidStack = new FluidStack(getTank().getFluidType(),
-                        Math.min(GeneralConfig.mbFlowRate, getTank().getFluidAmount()));
-				if(handler.fill(down.getOpposite(), fluidStack, false) > 0) {
-					int filled = handler.fill(down.getOpposite(), fluidStack, true);
-					drain(filled, true);
+			if(!worldObj.isAirBlock(xCoord + down.offsetX, yCoord + down.offsetY, zCoord + down.offsetZ)) {
+				// Try to fill block below
+				TileEntity tile = worldObj.getTileEntity(xCoord + down.offsetX, yCoord + down.offsetY, zCoord + down.offsetZ);
+				if (tile instanceof IFluidHandler) {
+					IFluidHandler handler = (IFluidHandler) tile;
+					FluidStack fluidStack = new FluidStack(getTank().getFluidType(),
+							Math.min(GeneralConfig.mbFlowRate, getTank().getFluidAmount()));
+					if (handler.fill(down.getOpposite(), fluidStack, false) > 0) {
+						int filled = handler.fill(down.getOpposite(), fluidStack, true);
+						drain(filled, true);
+					}
+				}
+			} else {
+				// Try to fill fluid container items below
+				List<EntityItem> items = worldObj.selectEntitiesWithinAABB(EntityItem.class,
+						AxisAlignedBB.getBoundingBox(
+								xCoord + down.offsetX, yCoord + down.offsetY, zCoord + down.offsetZ,
+								xCoord + down.offsetX + 1, yCoord + down.offsetY + 1, zCoord + down.offsetZ + 1
+						), IEntitySelector.selectAnything);
+				for(EntityItem item : items) {
+					if(item.getEntityItem() != null && item.getEntityItem().getItem() instanceof IFluidContainerItem &&
+							item.getEntityItem().stackSize == 1) {
+						ItemStack itemStack = item.getEntityItem().copy();
+						IFluidContainerItem container = (IFluidContainerItem) itemStack.getItem();
+						FluidStack fluidStack = new FluidStack(getTank().getFluidType(),
+								Math.min(GeneralConfig.mbFlowRate, getTank().getFluidAmount()));
+						if(container.fill(item.getEntityItem(), fluidStack, false) > 0) {
+							int filled = container.fill(itemStack, fluidStack, true);
+							drain(filled, true);
+							item.setEntityItemStack(itemStack);
+						}
+					}
 				}
 			}
 		}
