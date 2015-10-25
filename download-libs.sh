2aaa5e5bba37d5166ab7166cd8e0cfb6c456e647
Ruben Taelman <rubensworks@gmail.com>
2015-10-25 09:43:23 +0100

    Fix BloodMagic lib downloading

--- download-libs.sh
+++ download-libs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "libs_path=." > gradle.properties
-wget https://www.dropbox.com/sh/0aqvalqobu61t44/AADiG9psYrYuZncFcXzsnfm2a/BloodMagic-1.7.10-1.3.3-13-deobf.jar?dl=1 -O BloodMagic-1.7.10-1.3.3-13-deobf.jar
+wget https://www.dropbox.com/sh/0aqvalqobu61t44/AADbP4P3h0jTBmZlr0vvf-vDa/BloodMagic-1.7.10-1.3.3-13-deobf.jar?dl=1 -O BloodMagic-1.7.10-1.3.3-13-deobf.jar
