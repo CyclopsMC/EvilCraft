404029a06a0575ccbd7c0f621aef86ea32c0559e
Ruben Taelman <rubensworks@gmail.com>
2015-11-02 15:12:49 +0100

    Add safety for null directions in OrganicSpread

--- src/main/java/org/cyclops/evilcraft/core/algorithm/OrganicSpread.java
+++ src/main/java/org/cyclops/evilcraft/core/algorithm/OrganicSpread.java
@@ -1,7 +1,7 @@
 package org.cyclops.evilcraft.core.algorithm;
 
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.Vec3i;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import org.cyclops.cyclopscore.helper.LocationHelpers;
 
@@ -85,25 +85,31 @@ public class OrganicSpread {
         BlockPos newLocation = startLocation;
         
         // Safely get a random direction.
-        Vec3i direction = getRandomDirection();
-        
+        Vec3 direction = getRandomDirection();
+        int attempts = 10;
+        while(!isBigEnough(direction) && attempts > 0) {
+            direction = getRandomDirection();
+            attempts--;
+        }
+        if(!isBigEnough(direction)) return;
+
         // Copy old coordinates.
         BlockPos oldCoordinates = LocationHelpers.copyLocation(newLocation);
-        
+
         // Loop in that direction.
-        while(getSpreadable().isDone(world, newLocation) && isInArea(startLocation, oldCoordinates)) {
+        while (getSpreadable().isDone(world, newLocation) && isInArea(startLocation, oldCoordinates)) {
             // Calculate the new coordinates
-            BlockPos newCoordinates = oldCoordinates.add(direction);
-            
+            BlockPos newCoordinates = oldCoordinates.add(direction.xCoord, direction.yCoord, direction.zCoord);
+
             // Set the new coordinates
             newLocation = LocationHelpers.copyLocation(newCoordinates);
-            
+
             // Swap
             oldCoordinates = newCoordinates;
         }
-        
+
         // Spread to the new blockState.
-        if(!getSpreadable().isDone(world, newLocation)) {
+        if (!getSpreadable().isDone(world, newLocation)) {
             getSpreadable().spreadTo(world, newLocation);
         }
     }
@@ -111,13 +117,18 @@ public class OrganicSpread {
     protected boolean isInArea(BlockPos center, BlockPos location) {
         return Math.sqrt(center.distanceSq(location)) <= getRadius();
     }
+
+    protected boolean isBigEnough(Vec3 direction) {
+        float MIN = 0.3F;
+        return direction.xCoord >= MIN && direction.yCoord >= MIN && direction.zCoord >= MIN;
+    }
     
     /**
      * Get a random direction to spread in.
      * @return An array of a choice of -1;0;1 per coordinate index.
      */
-    protected Vec3i getRandomDirection() {
-        return new Vec3i((random.nextFloat() * 2 - 1) / 2, (random.nextFloat() * 2 - 1) / 2, (random.nextFloat() * 2 - 1) / 2);
+    protected Vec3 getRandomDirection() {
+        return new Vec3((random.nextFloat() * 2 - 1) / 2, (random.nextFloat() * 2 - 1) / 2, (random.nextFloat() * 2 - 1) / 2);
     }
 
     /**
