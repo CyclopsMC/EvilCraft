d5e6ff84c5522d073c97cc27890f148a09d31acb
Ruben Taelman <rubensworks@gmail.com>
2016-04-05 17:44:29 +0200

    Add config option to remove Brooms from loot chest spawning, Closes #351

--- src/main/java/evilcraft/item/BroomConfig.java
+++ src/main/java/evilcraft/item/BroomConfig.java
@@ -1,6 +1,8 @@
 package evilcraft.item;
 
 import evilcraft.client.render.item.RenderItemBroom;
+import evilcraft.core.config.ConfigurableProperty;
+import evilcraft.core.config.ConfigurableTypeCategory;
 import evilcraft.core.config.extendedconfig.ItemConfig;
 import evilcraft.core.helper.MinecraftHelpers;
 import evilcraft.proxy.ClientProxy;
@@ -18,6 +20,11 @@ public class BroomConfig extends ItemConfig {
      * The unique instance.
      */
     public static BroomConfig _instance;
+    /**
+     * If the broom should spawn in loot chests.
+     */
+    @ConfigurableProperty(category = ConfigurableTypeCategory.ITEM, comment = "If the broom should spawn in loot chests.")
+    public static boolean lootChests = true;
 
     /**
      * Make a new instance.
@@ -34,8 +41,10 @@ public class BroomConfig extends ItemConfig {
     @Override
     public void onRegistered() {
         super.onRegistered();
-        for(String chestCategory : MinecraftHelpers.CHESTGENCATEGORIES) {
-            ChestGenHooks.getInfo(chestCategory).addItem(new WeightedRandomChestContent(Broom.getInstance(), 0, 1, 1, 2));
+        if(BroomConfig.lootChests) {
+            for (String chestCategory : MinecraftHelpers.CHESTGENCATEGORIES) {
+                ChestGenHooks.getInfo(chestCategory).addItem(new WeightedRandomChestContent(Broom.getInstance(), 0, 1, 1, 2));
+            }
         }
         if (MinecraftHelpers.isClientSide()) {
             ClientProxy.ITEM_RENDERERS.put(this.getItemInstance(), new RenderItemBroom(this));
