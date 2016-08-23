CyclopsMC/EvilCraft

commit 604d40bb6940a9b33ad7f268346a2dfa12046952

Fix null entity renderer registration, Closes #467

src/main/java/org/cyclops/evilcraft/entity/item/EntityItemEmpowerableConfig.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import org.cyclops.cyclopscore.config.extendedconfig.EntityConfig;
 import org.cyclops.evilcraft.EvilCraft;
+import org.cyclops.evilcraft.core.client.render.RenderNull;
 
 /**
  * Config for the {@link EntityItemEmpowerable}.
@@ -39,6 +40,6 @@ public class EntityItemEmpowerableConfig extends EntityConfig<Entity> {
 
 	@Override
 	public Render<Entity> getRender(RenderManager renderManager, RenderItem renderItem) {
-		return null;
+		return new RenderNull(renderManager);
 	}
 }

src/main/java/org/cyclops/evilcraft/entity/item/EntityItemUndespawnableConfig.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import org.cyclops.cyclopscore.config.extendedconfig.EntityConfig;
 import org.cyclops.evilcraft.EvilCraft;
+import org.cyclops.evilcraft.core.client.render.RenderNull;
 
 /**
  * Config for the {@link EntityItemUndespawnable}.
@@ -39,6 +40,6 @@ public class EntityItemUndespawnableConfig extends EntityConfig<Entity> {
 
 	@Override
 	public Render<Entity> getRender(RenderManager renderManager, RenderItem renderItem) {
-		return null;
+        return new RenderNull(renderManager);
 	}
 }

commit 081d1e06140e1a5bcbce0adc22e375c714c0aa7a

Fix server crash because of render registration changes, Closes Introduced by 604d40bb6940a9b33ad7f268346a2dfa12046952

src/main/java/org/cyclops/evilcraft/entity/item/EntityItemEmpowerableConfig.java
@@ -4,6 +4,8 @@ import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.cyclops.cyclopscore.config.extendedconfig.EntityConfig;
 import org.cyclops.evilcraft.EvilCraft;
 import org.cyclops.evilcraft.core.client.render.RenderNull;
@@ -38,6 +40,7 @@ public class EntityItemEmpowerableConfig extends EntityConfig<Entity> {
         return true;
     }
 
+    @SideOnly(Side.CLIENT)
 	@Override
 	public Render<Entity> getRender(RenderManager renderManager, RenderItem renderItem) {
 		return new RenderNull(renderManager);

src/main/java/org/cyclops/evilcraft/entity/item/EntityItemUndespawnableConfig.java
@@ -4,6 +4,8 @@ import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.cyclops.cyclopscore.config.extendedconfig.EntityConfig;
 import org.cyclops.evilcraft.EvilCraft;
 import org.cyclops.evilcraft.core.client.render.RenderNull;
@@ -38,6 +40,7 @@ public class EntityItemUndespawnableConfig extends EntityConfig<Entity> {
         return true;
     }
 
+    @SideOnly(Side.CLIENT)
 	@Override
 	public Render<Entity> getRender(RenderManager renderManager, RenderItem renderItem) {
         return new RenderNull(renderManager);