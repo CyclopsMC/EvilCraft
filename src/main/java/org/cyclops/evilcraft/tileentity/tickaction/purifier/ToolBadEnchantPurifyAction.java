463a23db572a11c485e2e0c54f6e30a1265732b9
Ruben Taelman <rubensworks@gmail.com>
2017-01-29 10:56:20 +0100

    Allow purifier to remove cursed enchants

--- src/main/java/org/cyclops/evilcraft/tileentity/tickaction/purifier/ToolBadEnchantPurifyAction.java
+++ src/main/java/org/cyclops/evilcraft/tileentity/tickaction/purifier/ToolBadEnchantPurifyAction.java
@@ -1,5 +1,7 @@
 package org.cyclops.evilcraft.tileentity.tickaction.purifier;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.cyclops.cyclopscore.config.configurable.ConfigurableEnchantment;
@@ -7,6 +9,8 @@ import org.cyclops.cyclopscore.helper.EnchantmentHelpers;
 import org.cyclops.evilcraft.api.tileentity.purifier.IPurifierAction;
 import org.cyclops.evilcraft.tileentity.TilePurifier;
 import org.cyclops.evilcraft.tileentity.tickaction.bloodchest.DamageableItemRepairAction;
+
+import java.util.Map;
 
 /**
  * Purifier action to remove enchantments from tools.
@@ -29,11 +33,37 @@ public class ToolBadEnchantPurifyAction implements IPurifierAction {
     @Override
     public boolean canWork(TilePurifier tile) {
         if(!tile.getPurifyItem().isEmpty() && tile.getBucketsFloored() > 0) {
+            // Check curses
+            for (Map.Entry<Enchantment, Integer> entry : EnchantmentHelper.getEnchantments(tile.getPurifyItem()).entrySet()) {
+                if (entry.getKey().isCurse()) {
+                    return true;
+                }
+            }
+
+            // Check bad enchant registry
             for(ConfigurableEnchantment enchant : DamageableItemRepairAction.BAD_ENCHANTS) {
                 int enchantmentListID = EnchantmentHelpers.doesEnchantApply(tile.getPurifyItem(), enchant);
                 if (enchantmentListID >= 0) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    protected boolean removeEnchant(World world, TilePurifier tile, ItemStack purifyItem, int tick, Enchantment enchant) {
+        int enchantmentListID = EnchantmentHelpers.doesEnchantApply(purifyItem, enchant);
+        if(enchantmentListID > -1) {
+            if(tick >= PURIFY_DURATION) {
+                if(!world.isRemote) {
+                    int level = EnchantmentHelpers.getEnchantmentLevel(purifyItem, enchantmentListID);
+                    EnchantmentHelpers.setEnchantmentLevel(purifyItem, enchantmentListID, level - 1);
+                }
+                tile.setBuckets(tile.getBucketsFloored() - 1, tile.getBucketsRest());
+                return true;
+            }
+            if(world.isRemote) {
+                tile.showEffect();
             }
         }
         return false;
@@ -47,23 +77,17 @@ public class ToolBadEnchantPurifyAction implements IPurifierAction {
         World world = tile.getWorld();
         int tick = tile.getTick();
 
+        // Try removing curses
+        for (Map.Entry<Enchantment, Integer> entry : EnchantmentHelper.getEnchantments(tile.getPurifyItem()).entrySet()) {
+            if (!done && entry.getKey().isCurse()) {
+                done = removeEnchant(world, tile, purifyItem, tick, entry.getKey());
+            }
+        }
+
         // Try removing bad enchants.
         for(ConfigurableEnchantment enchant : DamageableItemRepairAction.BAD_ENCHANTS) {
             if(!done) {
-                int enchantmentListID = EnchantmentHelpers.doesEnchantApply(purifyItem, enchant);
-                if(enchantmentListID > -1) {
-                    if(tick >= PURIFY_DURATION) {
-                        if(!world.isRemote) {
-                            int level = EnchantmentHelpers.getEnchantmentLevel(purifyItem, enchantmentListID);
-                            EnchantmentHelpers.setEnchantmentLevel(purifyItem, enchantmentListID, level - 1);
-                        }
-                        tile.setBuckets(tile.getBucketsFloored() - 1, tile.getBucketsRest());
-                        done = true;
-                    }
-                    if(world.isRemote) {
-                        tile.showEffect();
-                    }
-                }
+                done = removeEnchant(world, tile, purifyItem, tick, enchant);
             }
         }
 
