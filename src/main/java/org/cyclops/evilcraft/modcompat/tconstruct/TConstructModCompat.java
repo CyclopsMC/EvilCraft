7fe2b36a71c0358564165f14491325a66013486c
Runesmacher <Runesmacher@gmail.com>
2017-02-15 22:48:24 +0100

    Added TCon Basin blood hardening

--- src/main/java/org/cyclops/evilcraft/modcompat/tconstruct/TConstructModCompat.java
+++ src/main/java/org/cyclops/evilcraft/modcompat/tconstruct/TConstructModCompat.java
@@ -9,8 +9,13 @@ import org.cyclops.evilcraft.Configs;
 import org.cyclops.evilcraft.EvilCraft;
 import org.cyclops.evilcraft.Reference;
 import org.cyclops.evilcraft.api.tileentity.bloodchest.IBloodChestRepairActionRegistry;
+import org.cyclops.evilcraft.block.HardenedBlood;
+import org.cyclops.evilcraft.block.HardenedBloodConfig;
+import org.cyclops.evilcraft.fluid.Blood;
+import org.cyclops.evilcraft.fluid.BloodConfig;
 import org.cyclops.evilcraft.item.EnderTearConfig;
 import slimeknights.tconstruct.library.TinkerRegistry;
+import slimeknights.tconstruct.library.smeltery.CastingRecipe;
 
 /**
  * Compatibility plugin for Tinkers' Construct.
@@ -21,30 +26,35 @@ public class TConstructModCompat implements IModCompat {
 
     @Override
     public String getModID() {
-       return Reference.MOD_TCONSTRUCT;
+        return Reference.MOD_TCONSTRUCT;
     }
 
     @Override
     public void onInit(IInitListener.Step step) {
-    	if(step == IInitListener.Step.POSTINIT) {
-    		EvilCraft._instance.getRegistryManager().getRegistry(IBloodChestRepairActionRegistry.class).
-    			register(new TConstructToolRepairTickAction());
+        if (step == IInitListener.Step.POSTINIT) {
+            EvilCraft._instance.getRegistryManager().getRegistry(IBloodChestRepairActionRegistry.class)
+                    .register(new TConstructToolRepairTickAction());
             Fluid ender = FluidRegistry.getFluid("Ender");
-            if(Configs.isEnabled(EnderTearConfig.class) && ender != null) {
+            if (Configs.isEnabled(EnderTearConfig.class) && ender != null) {
                 TinkerRegistry.registerMelting(new ItemStack(EnderTearConfig._instance.getItemInstance()), ender,
                         EnderTearConfig.mbLiquidEnder);
             }
-    	}
-    }
-    
-    @Override
-	public boolean isEnabled() {
-		return true;
-	}
 
-	@Override
-	public String getComment() {
-		return "Blood chest repair support for tinker tools.";
-	}
+            if (Configs.isEnabled(HardenedBloodConfig.class) && Configs.isEnabled(BloodConfig.class)) {
+                TinkerRegistry.registerBasinCasting(
+                        new CastingRecipe(new ItemStack(HardenedBlood.getInstance()), Blood.getInstance(), 1000, 400));
+            }
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getComment() {
+        return "Blood chest repair support for tinker tools.";
+    }
 
 }
