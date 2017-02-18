e9c69b4c58b6e680fb7770e97914c4cf893b2683
Runesmacher <Runesmacher@gmail.com>
2017-02-18 20:26:18 +0100

    Fixed TCon compatibilty issue

--- src/main/java/org/cyclops/evilcraft/modcompat/tconstruct/TConstructModCompat.java
+++ src/main/java/org/cyclops/evilcraft/modcompat/tconstruct/TConstructModCompat.java
@@ -1,21 +1,8 @@
 package org.cyclops.evilcraft.modcompat.tconstruct;
 
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
 import org.cyclops.cyclopscore.init.IInitListener;
 import org.cyclops.cyclopscore.modcompat.IModCompat;
-import org.cyclops.evilcraft.Configs;
-import org.cyclops.evilcraft.EvilCraft;
 import org.cyclops.evilcraft.Reference;
-import org.cyclops.evilcraft.api.tileentity.bloodchest.IBloodChestRepairActionRegistry;
-import org.cyclops.evilcraft.block.HardenedBlood;
-import org.cyclops.evilcraft.block.HardenedBloodConfig;
-import org.cyclops.evilcraft.fluid.Blood;
-import org.cyclops.evilcraft.fluid.BloodConfig;
-import org.cyclops.evilcraft.item.EnderTearConfig;
-import slimeknights.tconstruct.library.TinkerRegistry;
-import slimeknights.tconstruct.library.smeltery.CastingRecipe;
 
 /**
  * Compatibility plugin for Tinkers' Construct.
@@ -32,18 +19,7 @@ public class TConstructModCompat implements IModCompat {
     @Override
     public void onInit(IInitListener.Step step) {
         if (step == IInitListener.Step.POSTINIT) {
-            EvilCraft._instance.getRegistryManager().getRegistry(IBloodChestRepairActionRegistry.class)
-                    .register(new TConstructToolRepairTickAction());
-            Fluid ender = FluidRegistry.getFluid("Ender");
-            if (Configs.isEnabled(EnderTearConfig.class) && ender != null) {
-                TinkerRegistry.registerMelting(new ItemStack(EnderTearConfig._instance.getItemInstance()), ender,
-                        EnderTearConfig.mbLiquidEnder);
-            }
-
-            if (Configs.isEnabled(HardenedBloodConfig.class) && Configs.isEnabled(BloodConfig.class)) {
-                TinkerRegistry.registerBasinCasting(
-                        new CastingRecipe(new ItemStack(HardenedBlood.getInstance()), Blood.getInstance(), 1000, 400));
-            }
+            TConstructRecipeManager.register();
         }
     }
 
