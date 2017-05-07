bdd33ba703fde5b9eb523ca7aeb7225902fce4d8
Ruben Taelman <rubensworks@gmail.com>
2017-05-07 08:52:28 +0200

    Add Exalted Crafter item handler capability

--- src/main/java/org/cyclops/evilcraft/item/ExaltedCrafter.java
+++ src/main/java/org/cyclops/evilcraft/item/ExaltedCrafter.java
@@ -10,12 +10,23 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.cyclops.cyclopscore.config.extendedconfig.ExtendedConfig;
 import org.cyclops.cyclopscore.config.extendedconfig.ItemConfig;
+import org.cyclops.cyclopscore.helper.ItemStackHelpers;
+import org.cyclops.cyclopscore.helper.MinecraftHelpers;
 import org.cyclops.cyclopscore.inventory.NBTSimpleInventoryItemHeld;
 import org.cyclops.cyclopscore.item.ItemGui;
 import org.cyclops.evilcraft.client.gui.container.GuiExaltedCrafter;
@@ -147,5 +158,164 @@ public class ExaltedCrafter extends ItemGui implements IItemEmpowerable {
     @SideOnly(Side.CLIENT)
     public Class<? extends GuiScreen> getGui() {
         return GuiExaltedCrafter.class;
+    }
+
+    @Override
+    public ICapabilityProvider initCapabilities(final ItemStack stack, NBTTagCompound nbt) {
+        final IItemHandler itemHandler= new ItemHandler(stack);
+        return new ICapabilityProvider() {
+            @Override
+            public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+                return isWooden(stack) && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY;
+            }
+
+            @Override
+            public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+                return isWooden(stack) && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY
+                        ? (T) itemHandler : null;
+            }
+        };
+    }
+
+    public class ItemHandler implements IItemHandlerModifiable {
+
+        protected ItemStack itemStack;
+
+        public ItemHandler(ItemStack itemStack) {
+            this.itemStack = itemStack;
+        }
+
+        protected ItemStack[] getItemList() {
+            ItemStack[] itemStacks = new ItemStack[getSlots()];
+            NBTTagCompound rootTag = itemStack.getTagCompound();
+            if (rootTag != null && rootTag.hasKey(NBTSimpleInventoryItemHeld.NBT_TAG_ROOT, MinecraftHelpers.NBTTag_Types.NBTTagList.ordinal())) {
+                NBTTagList nbttaglist = rootTag.getTagList(NBTSimpleInventoryItemHeld.NBT_TAG_ROOT, MinecraftHelpers.NBTTag_Types.NBTTagCompound.ordinal());
+                for (int j = 0; j < nbttaglist.tagCount(); ++j) {
+                    NBTTagCompound slot = nbttaglist.getCompoundTagAt(j);
+                    int index;
+                    if (slot.hasKey("index")) {
+                        index = slot.getInteger("index");
+                    } else {
+                        index = slot.getByte("Slot");
+                    }
+                    if (index >= 0 && index < getSlots()) {
+                        itemStacks[index] = ItemStack.loadItemStackFromNBT(slot);
+                    }
+                }
+            }
+            return itemStacks;
+        }
+
+        protected void setItemList(ItemStack[] itemStacks) {
+            NBTTagCompound rootTag = ItemStackHelpers.getSafeTagCompound(itemStack);
+            NBTTagList slots = new NBTTagList();
+            for (byte index = 0; index < getSlots(); ++index) {
+                ItemStack itemStack = itemStacks[index];
+                if (itemStack != null && itemStack.stackSize > 0) {
+                    NBTTagCompound slot = new NBTTagCompound();
+                    slots.appendTag(slot);
+                    slot.setByte("Slot", index);
+                    itemStack.writeToNBT(slot);
+                }
+            }
+            rootTag.setTag(NBTSimpleInventoryItemHeld.NBT_TAG_ROOT, slots);
+        }
+
+        @Override
+        public void setStackInSlot(int slot, ItemStack stack) {
+            ItemStack[] itemStacks = getItemList();
+            itemStacks[slot] = stack;
+            setItemList(itemStacks);
+        }
+
+        @Override
+        public int getSlots() {
+            return 27;
+        }
+
+        @Override
+        public ItemStack getStackInSlot(int slot) {
+            return getItemList()[slot];
+        }
+
+        @Override
+        public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+            if (stack == null) {
+                return null;
+            }
+
+            ItemStack[] itemStacks = getItemList();
+            ItemStack existingStack = itemStacks[slot];
+
+            int maxStackSize;
+            if (existingStack != null) {
+                if (!ItemHandlerHelper.canItemStacksStack(stack, existingStack))
+                    return stack;
+
+                maxStackSize = Math.min(stack.getMaxStackSize(), getSlotLimit(slot)) - existingStack.stackSize;
+
+                if (stack.stackSize <= maxStackSize) {
+                    if (!simulate) {
+                        ItemStack copy = stack.copy();
+                        copy.stackSize += existingStack.stackSize;
+                        setStackInSlot(slot, copy);
+                    }
+
+                    return null;
+                } else  {
+                    // copy the stack to not modify the original one
+                    stack = stack.copy();
+                    if (!simulate) {
+                        ItemStack copy = stack.splitStack(maxStackSize);
+                        copy.stackSize += existingStack.stackSize;
+                        setStackInSlot(slot, copy);
+                        return stack;
+                    }  else {
+                        stack.stackSize -= maxStackSize;
+                        return stack;
+                    }
+                }
+            } else {
+                maxStackSize = Math.min(stack.getMaxStackSize(), getSlotLimit(slot));
+                if (maxStackSize < stack.stackSize) {
+                    // copy the stack to not modify the original one
+                    stack = stack.copy();
+                    if (!simulate) {
+                        setStackInSlot(slot, stack.splitStack(maxStackSize));
+                        return stack;
+                    } else {
+                        stack.stackSize -= maxStackSize;
+                        return stack;
+                    }
+                } else {
+                    if (!simulate) {
+                        setStackInSlot(slot, stack);
+                    }
+                    return null;
+                }
+            }
+        }
+
+        @Override
+        public ItemStack extractItem(int slot, int amount, boolean simulate) {
+            if (amount == 0)
+                return null;
+
+            ItemStack existingStack = getStackInSlot(slot);
+
+            if (existingStack == null) {
+                return null;
+            }
+
+            ItemStack extracted = existingStack.splitStack(amount);
+            if (!simulate) {
+                setStackInSlot(slot, existingStack);
+            }
+            return extracted;
+        }
+
+        private int getSlotLimit(int slot) {
+            return 64;
+        }
     }
 }
