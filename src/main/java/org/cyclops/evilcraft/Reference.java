6f05e435ae6e54fdc72d35f9016cc6f417b2a95e
Ruben Taelman <rubensworks@gmail.com>
2017-05-19 19:42:13 +0200

    Bump mod version

--- src/main/java/org/cyclops/evilcraft/Reference.java
+++ src/main/java/org/cyclops/evilcraft/Reference.java
@@ -77,7 +77,7 @@ public class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "13.20.0.2282";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.2";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.3";
     public static final String MOD_THERMALEXPANSION = "thermalexpansion";
     public static final String MOD_WAILA = "Waila";
     public static final String MOD_FORESTRY = "forestry";
