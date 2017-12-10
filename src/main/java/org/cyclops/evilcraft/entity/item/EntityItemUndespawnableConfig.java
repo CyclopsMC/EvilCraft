984e7070e5b829e3df7aad2803f5d656e8b3e773
Ruben Taelman <rubensworks@gmail.com>
2017-12-10 19:44:53 +0100

    Fix Exalted Crafter not rendering as entity, Closes #618

--- src/main/java/org/cyclops/evilcraft/entity/item/EntityItemUndespawnableConfig.java
+++ src/main/java/org/cyclops/evilcraft/entity/item/EntityItemUndespawnableConfig.java
@@ -2,20 +2,20 @@ package org.cyclops.evilcraft.entity.item;
 
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderEntityItem;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.cyclops.cyclopscore.config.extendedconfig.EntityConfig;
 import org.cyclops.evilcraft.EvilCraft;
-import org.cyclops.evilcraft.core.client.render.RenderNull;
 
 /**
  * Config for the {@link EntityItemUndespawnable}.
  * @author rubensworks
  *
  */
-public class EntityItemUndespawnableConfig extends EntityConfig<Entity> {
+public class EntityItemUndespawnableConfig extends EntityConfig<EntityItem> {
     
     /**
      * The unique instance.
@@ -42,7 +42,7 @@ public class EntityItemUndespawnableConfig extends EntityConfig<Entity> {
 
     @SideOnly(Side.CLIENT)
 	@Override
-	public Render<Entity> getRender(RenderManager renderManager, RenderItem renderItem) {
-        return new RenderNull(renderManager);
+	public Render<EntityItem> getRender(RenderManager renderManager, RenderItem renderItem) {
+        return new RenderEntityItem(renderManager, renderItem);
 	}
 }
