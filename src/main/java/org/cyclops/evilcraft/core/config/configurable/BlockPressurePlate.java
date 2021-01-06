c01c25f1c66b062e087f447180e2d17a5966dc19
Ruben Taelman <rubensworks@gmail.com>
2021-01-06 19:52:29 +0100

    Fix spiked plate not dropping when block under breaks
    Closes #824

--- src/main/java/org/cyclops/evilcraft/core/config/configurable/BlockPressurePlate.java
+++ src/main/java/org/cyclops/evilcraft/core/config/configurable/BlockPressurePlate.java
@@ -6,8 +6,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 /**
  * {@link AbstractPressurePlateBlock} that can hold ExtendedConfigs.
@@ -27,14 +25,6 @@ public abstract class BlockPressurePlate extends AbstractPressurePlateBlock {
     @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
         builder.add(POWERED);
-    }
-
-    @Override
-    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
-        if(!isValidPosition(state, worldIn, pos)) {
-            worldIn.removeBlock(pos, false);
-        }
     }
 
 }
