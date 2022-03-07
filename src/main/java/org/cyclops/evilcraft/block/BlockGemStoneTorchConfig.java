7fa02f4b5df0e62a096be1456ec9362c58d4a02f
Ruben Taelman <rubensworks@gmail.com>
2022-03-07 19:10:10 +0100

    Fix gemstone torch not being placeable on walls

--- src/main/java/org/cyclops/evilcraft/block/BlockGemStoneTorchConfig.java
+++ src/main/java/org/cyclops/evilcraft/block/BlockGemStoneTorchConfig.java
@@ -1,18 +1,20 @@
 package org.cyclops.evilcraft.block;
 
-
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.StandingAndWallBlockItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.TorchBlock;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.core.particles.ParticleTypes;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.cyclops.cyclopscore.config.ConfigurableProperty;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockConfig;
 import org.cyclops.evilcraft.EvilCraft;
+import org.cyclops.evilcraft.RegistryEntries;
 
 /**
  * Config for the Burning Gemstone Torch.
@@ -33,7 +35,10 @@ public class BlockGemStoneTorchConfig extends BlockConfig {
                         .strength(0)
                         .lightLevel((state) -> 14)
                         .sound(SoundType.WOOD), ParticleTypes.FLAME),
-                getDefaultItemConstructor(EvilCraft._instance)
+                (eConfig, block) -> {
+                    Item.Properties itemProperties = new Item.Properties().tab(EvilCraft._instance.getDefaultItemGroup());
+                    return new StandingAndWallBlockItem(block, RegistryEntries.BLOCK_GEM_STONE_TORCH_WALL, itemProperties);
+                }
         );
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
     }
