3af7daaed0dffbdaae17cd8a843522199443bbc6
Ruben Taelman <rubensworks@gmail.com>
2022-05-18 17:32:42 +0200

    Fix Dark Tank data being lost when breaking, Closes #911
    Closes #914

--- src/main/java/org/cyclops/evilcraft/loot/functions/LootFunctionCopyTankData.java
+++ src/main/java/org/cyclops/evilcraft/loot/functions/LootFunctionCopyTankData.java
@@ -4,19 +4,19 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootFunction;
 import net.minecraft.loot.LootFunctionType;
-import net.minecraft.loot.conditions.ILootCondition;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.loot.LootContext;
 import net.minecraft.loot.LootParameters;
-import net.minecraft.loot.functions.ILootFunction;
+import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import org.cyclops.cyclopscore.capability.fluid.IFluidHandlerItemCapacity;
 import org.cyclops.cyclopscore.capability.fluid.IFluidHandlerMutable;
+import org.cyclops.cyclopscore.fluid.SingleUseTank;
 import org.cyclops.cyclopscore.helper.LootHelpers;
 import org.cyclops.evilcraft.Reference;
+import org.cyclops.evilcraft.core.tileentity.TankInventoryTileEntity;
 
 /**
  * Copies fluid tank data to the item.
@@ -31,18 +31,16 @@ public class LootFunctionCopyTankData extends LootFunction {
 
     @Override
     public ItemStack doApply(ItemStack itemStack, LootContext lootContext) {
-        TileEntity tile = lootContext.get(LootParameters.BLOCK_ENTITY);
-        tile.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY)
-                .ifPresent(fluidHandlerTile -> {
-                    itemStack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY)
-                            .ifPresent(fluidHandlerItem -> {
-                                if (fluidHandlerItem instanceof IFluidHandlerMutable) {
-                                    ((IFluidHandlerMutable) fluidHandlerItem).setFluidInTank(0, fluidHandlerTile.getFluidInTank(0));
-                                }
-                                if (fluidHandlerItem instanceof IFluidHandlerItemCapacity) {
-                                    ((IFluidHandlerItemCapacity) fluidHandlerItem).setCapacity(fluidHandlerTile.getTankCapacity(0));
-                                }
-                            });
+        TankInventoryTileEntity tile = (TankInventoryTileEntity) lootContext.get(LootParameters.BLOCK_ENTITY);
+        SingleUseTank fluidHandlerTile = tile.getTank();
+        itemStack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY)
+                .ifPresent(fluidHandlerItem -> {
+                    if (fluidHandlerItem instanceof IFluidHandlerMutable) {
+                        ((IFluidHandlerMutable) fluidHandlerItem).setFluidInTank(0, fluidHandlerTile.getFluidInTank(0));
+                    }
+                    if (fluidHandlerItem instanceof IFluidHandlerItemCapacity) {
+                        ((IFluidHandlerItemCapacity) fluidHandlerItem).setCapacity(fluidHandlerTile.getTankCapacity(0));
+                    }
                 });
         return itemStack;
     }
