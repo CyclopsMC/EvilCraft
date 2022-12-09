6433bd08b93bf2b490975d003ea1cad9620a0c8b
Ruben Taelman <rubensworks@gmail.com>
2022-12-09 15:39:14 +0100

    Remove unused maxTankSize config option, Closes #953
    Max tank sizes are modified via datapacks in recipes directly now.

--- src/main/java/org/cyclops/evilcraft/block/BlockDarkTankConfig.java
+++ src/main/java/org/cyclops/evilcraft/block/BlockDarkTankConfig.java
@@ -20,9 +20,6 @@ import org.cyclops.evilcraft.core.item.ItemBlockFluidContainer;
  */
 public class BlockDarkTankConfig extends BlockConfig {
 
-    @ConfigurableProperty(category = "machine", comment = "The maximum tank size possible by combining tanks. (Make sure that you do not cross the max int size.)")
-    public static int maxTankSize = 65536000;
-
     @ConfigurableProperty(category = "machine", comment = "The maximum tank size visible in the creative tabs. (Make sure that you do not cross the max int size.)")
     public static int maxTankCreativeSize = 4096000;
 
