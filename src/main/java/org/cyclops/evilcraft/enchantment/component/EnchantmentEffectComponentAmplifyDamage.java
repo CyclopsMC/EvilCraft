2e8fca1e3565220f6bf0d5b1c4e89d17e4e1472f
Ruben Taelman <rubensworks@gmail.com>
2024-07-20 14:29:23 +0200

    Fix machine gun skeletons
    This fixes the problems where all skeletons would skip their attack
    delay.
    
    Closes #1033

--- src/main/java/org/cyclops/evilcraft/enchantment/component/EnchantmentEffectComponentAmplifyDamage.java
+++ src/main/java/org/cyclops/evilcraft/enchantment/component/EnchantmentEffectComponentAmplifyDamage.java
@@ -9,6 +9,7 @@ import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import org.cyclops.cyclopscore.helper.EnchantmentHelpers;
 import org.cyclops.evilcraft.RegistryEntries;
+import org.cyclops.evilcraft.core.algorithm.Wrapper;
 
 import java.util.Random;
 
@@ -18,14 +19,15 @@ import java.util.Random;
 public class EnchantmentEffectComponentAmplifyDamage {
 
     @SubscribeEvent(priority = EventPriority.NORMAL)
-    public void breakingEvent(LivingDamageEvent.Post event) {
+    public void livingDamage(LivingDamageEvent.Post event) {
         if (!event.getEntity().getCommandSenderWorld().isClientSide() && event.getSource().getEntity() instanceof LivingEntity) {
             LivingEntity entity = (LivingEntity) event.getSource().getEntity();
             InteractionHand hand = entity.getUsedItemHand();
             if (hand != null) {
                 ItemStack itemStack = entity.getItemInHand(hand);
-                amplifyDamage(itemStack);
-                entity.setItemInHand(hand, itemStack);
+                if (amplifyDamage(itemStack)) {
+                    entity.setItemInHand(hand, itemStack);
+                }
             }
         }
     }
@@ -36,13 +38,15 @@ public class EnchantmentEffectComponentAmplifyDamage {
             InteractionHand hand = event.getPlayer().getUsedItemHand();
             if (hand != null) {
                 ItemStack itemStack = event.getPlayer().getItemInHand(hand);
-                amplifyDamage(itemStack);
-                event.getPlayer().setItemInHand(hand, itemStack);
+                if (amplifyDamage(itemStack)) {
+                    event.getPlayer().setItemInHand(hand, itemStack);
+                }
             }
         }
     }
 
-    public void amplifyDamage(ItemStack itemStack) {
+    public boolean amplifyDamage(ItemStack itemStack) {
+        Wrapper<Boolean> applied = new Wrapper<>(false);
         EnchantmentHelpers.runIterationOnItem(itemStack, (enchantment, level) -> {
             int modifier = enchantment.value().effects().getOrDefault(RegistryEntries.ENCHANTMENTEFFECT_COMPONENT_AMPLIFY_DAMAGE.get(), 0);
             if (modifier > 0) {
@@ -51,9 +55,11 @@ public class EnchantmentEffectComponentAmplifyDamage {
                 if(random.nextFloat() < 0.6F ? false : random.nextInt(level + 1) > 0
                         && newDamage <= itemStack.getMaxDamage()) {
                     itemStack.setDamageValue(newDamage);
+                    applied.set(true);
                 }
             }
         });
+        return applied.get();
     }
 
 }
