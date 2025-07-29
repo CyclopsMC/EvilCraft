6c06fefb2e8e17a969f795ac711290be6dc2e60e
Ruben Taelman <rubensworks@gmail.com>
2025-07-29 20:15:32 +0200

    Fix broom parts not showing in creative tabs

--- src/main/java/org/cyclops/evilcraft/item/ItemBroomPartConfig.java
+++ src/main/java/org/cyclops/evilcraft/item/ItemBroomPartConfig.java
@@ -1,16 +1,19 @@
 package org.cyclops.evilcraft.item;
 
 import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.cyclops.cyclopscore.config.extendedconfig.ItemConfig;
 import org.cyclops.cyclopscore.helper.MinecraftHelpers;
 import org.cyclops.evilcraft.EvilCraft;
 
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Config for the {@link ItemBroomPart}.
@@ -26,6 +29,7 @@ public class ItemBroomPartConfig extends ItemConfig {
                 eConfig -> new ItemBroomPart(new Item.Properties()
                         )
         );
+        EvilCraft._instance.getModEventBus().addListener(this::fillCreativeTab);
         if (MinecraftHelpers.isClientSide()) {
             EvilCraft._instance.getModEventBus().addListener(this::registerColors);
         }
@@ -38,6 +42,19 @@ public class ItemBroomPartConfig extends ItemConfig {
 
     @Override
     protected Collection<ItemStack> getDefaultCreativeTabEntries() {
+        // Register items dynamically into tab, because when this is called, capabilities are not initialized yet.
+        return Collections.emptyList();
+    }
+
+    protected void fillCreativeTab(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTab() == EvilCraft._instance.getDefaultCreativeTab()) {
+            for (ItemStack itemStack : dynamicCreativeTabEntries()) {
+                event.accept(itemStack, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }
+        }
+    }
+
+    protected Collection<ItemStack> dynamicCreativeTabEntries() {
         NonNullList<ItemStack> list = NonNullList.create();
         ((ItemBroomPart) getInstance()).fillItemCategory(list);
         return list;
