70260309bf8ae0f38ac1c3ecde7932279d912e84
Ruben Taelman <rubensworks@gmail.com>
2026-03-07 09:29:27 +0100

    Fix Entangled Chalice rendering fluid incorrectly in item-form
    Closes #1165

--- src/main/java/org/cyclops/evilcraft/block/BlockEntangledChaliceConfigClient.java
+++ src/main/java/org/cyclops/evilcraft/block/BlockEntangledChaliceConfigClient.java
@@ -1,14 +1,10 @@
 package org.cyclops.evilcraft.block;
 
-import net.minecraft.resources.Identifier;
 import net.neoforged.neoforge.client.event.RegisterBlockStateModels;
 import net.neoforged.neoforge.client.event.RegisterItemModelsEvent;
-import net.neoforged.neoforge.client.event.RegisterSpecialModelRendererEvent;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockClientConfig;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockConfigCommon;
 import org.cyclops.cyclopscore.init.ModBaseNeoForge;
-import org.cyclops.evilcraft.Reference;
-import org.cyclops.evilcraft.client.render.blockentity.RenderItemStackBlockEntityEntangledChalice;
 import org.cyclops.evilcraft.client.render.model.ItemModelEntangledChalice;
 import org.cyclops.evilcraft.client.render.model.ModelEntangledChaliceBaked;
 
@@ -18,7 +14,6 @@ import org.cyclops.evilcraft.client.render.model.ModelEntangledChaliceBaked;
 public class BlockEntangledChaliceConfigClient extends BlockClientConfig<ModBaseNeoForge<?>> {
     public BlockEntangledChaliceConfigClient(BlockConfigCommon<ModBaseNeoForge<?>> blockConfig) {
         super(blockConfig);
-        blockConfig.getMod().getModEventBus().addListener((RegisterSpecialModelRendererEvent event) -> event.register(Identifier.fromNamespaceAndPath(Reference.MOD_ID, "entangled_chalice_fluid_overlay"), RenderItemStackBlockEntityEntangledChalice.Unbaked.MAP_CODEC));
         blockConfig.getMod().getModEventBus().addListener((RegisterItemModelsEvent event) -> event.register(ItemModelEntangledChalice.Unbaked.ID, ItemModelEntangledChalice.Unbaked.MAP_CODEC));
         blockConfig.getMod().getModEventBus().addListener((RegisterBlockStateModels event) -> event.registerModel(ModelEntangledChaliceBaked.Unbaked.ID, ModelEntangledChaliceBaked.Unbaked.CODEC));
     }
